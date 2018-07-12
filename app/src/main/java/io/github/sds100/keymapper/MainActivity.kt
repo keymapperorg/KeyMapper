1c48a16e3bc2314cac7f8cb956127abeea52ae4b
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-12 12:08:31 +0100

    reconfigured the default fab

--- app/src/main/java/io/github/sds100/keymapper/MainActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/MainActivity.kt
@@ -1,11 +1,9 @@
 package io.github.sds100.keymapper
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fabNewKeyMap.setOnClickListener {
+            
         }
     }
 
