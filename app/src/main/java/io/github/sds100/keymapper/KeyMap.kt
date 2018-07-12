2a5ea47a5f6793c483d6185987d9a93219ad252a
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-12 18:11:26 +0100

    Created KeyMap object

---
+++ app/src/main/java/io/github/sds100/keymapper/KeyMap.kt
@@ -0,0 +1,12 @@
+package io.github.sds100.keymapper
+
+/**
+ * Created by sds100 on 12/07/2018.
+ */
+
+/**
+ * [id] the unique identifier for this keymap
+ */
+data class KeyMap(
+        val id: Long
+)
