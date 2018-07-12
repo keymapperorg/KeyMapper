c7e03764e7d984cd8d9053843e8c6fafa233e111
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-12 16:35:49 +0100

    created KeyMapRecyclerViewAdapter

---
+++ app/src/main/java/io/github/sds100/keymapper/KeyMapRecyclerViewAdapter.kt
@@ -0,0 +1,27 @@
+package io.github.sds100.keymapper
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by sds100 on 12/07/2018.
+ */
+class KeyMapRecyclerViewAdapter : RecyclerView.Adapter<KeyMapRecyclerViewAdapter.ViewHolder>() {
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
