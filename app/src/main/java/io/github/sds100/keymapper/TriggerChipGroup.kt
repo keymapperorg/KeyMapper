21b3a7e29d434bc25f4efc6099a991284028c98f
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-13 14:52:52 +0100

    Created TriggerChipGroup class

---
+++ app/src/main/java/io/github/sds100/keymapper/TriggerChipGroup.kt
@@ -0,0 +1,24 @@
+package io.github.sds100.keymapper
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.chip.ChipGroup
+
+/**
+ * Created by sds100 on 13/07/2018.
+ */
+
+/**
+ * Controls displaying triggers as chips
+ * @see Chip
+ * @see ChipGroup
+ */
+class TriggerChipGroup(
+        context: Context?,
+        attrs: AttributeSet?
+) : ChipGroup(context, attrs) {
+
+    fun addTrigger() {
+
+    }
+}
