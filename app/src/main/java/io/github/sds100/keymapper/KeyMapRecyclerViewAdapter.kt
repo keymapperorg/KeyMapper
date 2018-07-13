4ad22e89b9c3049c75fff748e1f1344037cc227e
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-13 14:53:07 +0100

    Created KeyMapRecyclerViewAdapter and view holder layout

---
+++ app/src/main/java/io/github/sds100/keymapper/KeyMapRecyclerViewAdapter.kt
@@ -0,0 +1,32 @@
+package io.github.sds100.keymapper
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by sds100 on 12/07/2018.
+ */
+class KeyMapRecyclerViewAdapter(private val keyMapList: List<KeyMap>
+) : RecyclerView.Adapter<KeyMapRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return ViewHolder(inflater.inflate(R.layout.keymap_adapter_item, null))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.itemView.apply {
+
+        }
+    }
+
+    override fun getItemCount() = keyMapList.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
