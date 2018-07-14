e1b3def7c3c61f68c85a42310ee8eb86682c6fc2
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-14 16:38:15 +0100

    NewKeyMapActivity: show a dialog when the user attempts to leave

--- app/src/main/java/io/github/sds100/keymapper/NewKeyMapActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/NewKeyMapActivity.kt
@@ -1,8 +1,10 @@
 package io.github.sds100.keymapper
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_new_key_map.*
+import org.jetbrains.anko.alert
 
 class NewKeyMapActivity : AppCompatActivity() {
 
@@ -10,5 +12,30 @@ class NewKeyMapActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_key_map)
         setSupportActionBar(toolbar)
+
+        //show the back button in the toolbar
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onBackPressed() {
+        //ask the user whether they are sure they want to leave
+        alert {
+            title = getString(R.string.dialog_title_are_you_sure)
+            message = getString(R.string.dialog_message_are_you_sure_cancel_new_key_map)
+            positiveButton(R.string.pos_yes, onClicked = { super.onBackPressed() })
+            negativeButton(R.string.neg_no, onClicked = { dialog -> dialog.cancel() })
+        }.show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+        /*when the back button in the toolbar is pressed, call onBackPressed so it acts like the
+        hardware back button */
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
