36100730d367d42796b12141f67796f09104f636
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-16 16:38:03 +0100

    Created Action class

---
+++ app/src/main/java/io/github/sds100/keymapper/Action.kt
@@ -0,0 +1,34 @@
+package io.github.sds100.keymapper
+
+/**
+ * Created by sds100 on 16/07/2018.
+ */
+
+/**
+ * @property [data] The information required to perform the action. E.g if the type is [TYPE_APP]
+ * then the data will be the package name of the application
+ *
+ * Different Types of actions:
+ * - Applications
+ * - Application shortcuts
+ * - Keycode
+ * - Insert a block of text
+ * - System settings
+ * - Root system settings
+ * - Global actions (performGlobalAction)
+ */
+
+data class Action(
+        val type: Int,
+        val data: String
+){
+    companion object {
+        const val TYPE_APP = 0
+        const val TYPE_APP_SHORTCUT = 1
+        const val TYPE_KEY_CODE = 2
+        const val TYPE_TEXT_BLOCK = 3
+        const val TYPE_SYSTEM_SETTING = 4
+        const val TYPE_ROOT_SYSTEM_SETTING = 5
+        const val TYPE_GLOBAL_ACTION = 6
+    }
+}
--- app/src/main/java/io/github/sds100/keymapper/KeyMap.kt
+++ app/src/main/java/io/github/sds100/keymapper/KeyMap.kt
@@ -4,11 +4,8 @@ package io.github.sds100.keymapper
  * Created by sds100 on 12/07/2018.
  */
 
-/**
- *
- * [id] the unique identifier for this keymap
- */
-
 data class KeyMap(
-        val id: Long
+        val id: Long,
+        val triggerList: MutableList<Trigger> = mutableListOf(),
+        val action: Action
 )
