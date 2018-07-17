0f375357908182a32f2d8d852f2e1d44dbff0c41
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-17 14:12:11 +0100

    ChooseActionActivity: Display action types in the spinner

---
+++ app/src/main/java/io/github/sds100/keymapper/ActionTypeSpinnerAdapter.kt
@@ -0,0 +1,46 @@
+package io.github.sds100.keymapper
+
+import android.content.Context
+import android.widget.ArrayAdapter
+
+/**
+ * Created by sds100 on 16/07/2018.
+ */
+class ActionTypeSpinnerAdapter(
+        context: Context
+) : ArrayAdapter<String>(
+        context,
+        android.R.layout.simple_spinner_item,
+        context.resources.getStringArray(R.array.spinner_action_type_array)
+) {
+
+    companion object {
+        /**
+         * Must be the same order as [R.array.spinner_action_type_array]
+         */
+        private val ORDERED_ITEM_TYPE_LIST = listOf(
+                Item.APP,
+                Item.APP_SHORTCUT,
+                Item.KEY,
+                Item.ACTION,
+                Item.SETTING,
+                Item.TEXT_BLOCK,
+                Item.KEYCODE
+        )
+
+        fun getItemTypeFromPosition(position: Int): Item {
+            return ORDERED_ITEM_TYPE_LIST[position]
+        }
+    }
+
+    init {
+        setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    }
+
+    /**
+     *
+     */
+    enum class Item {
+        APP, APP_SHORTCUT, KEY, ACTION, SETTING, TEXT_BLOCK, KEYCODE
+    }
+}
