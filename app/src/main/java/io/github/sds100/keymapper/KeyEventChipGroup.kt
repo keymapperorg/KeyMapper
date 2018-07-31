be49671003a8c16b09738ed9d4e89825bd88f13d
sds2001 <seth.d.schroeder@gmail.com>
2018-07-31 09:48:44 +0100

    Extend TriggerChipGroup from KeyEventChipGroup

---
+++ app/src/main/java/io/github/sds100/keymapper/KeyEventChipGroup.kt
@@ -0,0 +1,85 @@
+package io.github.sds100.keymapper
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.KeyEvent
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+/**
+ * Created by sds100 on 13/07/2018.
+ */
+
+/**
+ * Controls displaying a key event as a chip
+ * @see Chip
+ * @see ChipGroup
+ */
+open class KeyEventChipGroup(
+        context: Context?,
+        attrs: AttributeSet?) : ChipGroup(context, attrs) {
+
+    companion object {
+
+        /**
+         * Maps keys which aren't single characters like the Control keys to a string representation
+         */
+        private val NON_CHARACTER_KEY_MAP = mapOf(
+                Pair(KeyEvent.KEYCODE_VOLUME_DOWN, "Vol Down"),
+                Pair(KeyEvent.KEYCODE_VOLUME_UP, "Vol Up"),
+
+                Pair(KeyEvent.KEYCODE_CTRL_LEFT, "Ctrl"),
+                Pair(KeyEvent.KEYCODE_CTRL_RIGHT, "Ctrl"),
+
+                Pair(KeyEvent.KEYCODE_SHIFT_LEFT, "Shift"),
+                Pair(KeyEvent.KEYCODE_SHIFT_RIGHT, "Shift"),
+
+                Pair(KeyEvent.KEYCODE_DPAD_LEFT, "Left")
+        )
+    }
+
+    /**
+     * The chips currently being shown in the chip group
+     */
+    protected val chips = mutableListOf<KeyChip>()
+
+    /**
+     * Create a new chip view and show it in the group
+     */
+    fun addChip(event: KeyEvent) {
+        val chip = KeyChip(context, event.keyCode)
+        val text = createTextFromKeyEvent(event)
+
+        chip.text = text
+
+        chips.add(chip)
+
+        addView(chip)
+    }
+
+    fun removeChip(keyCode: Int) {
+        val chip = chips.find { it.keyCode == keyCode }
+
+        removeView(chip)
+        chips.remove(chip)
+    }
+
+    fun removeAllChips() {
+        removeAllViews()
+        chips.clear()
+    }
+
+    /**
+     * Create a text representation of a key event. E.g if the control key was pressed,
+     * "Ctrl" will be returned
+     */
+    private fun createTextFromKeyEvent(event: KeyEvent): String {
+
+        if (NON_CHARACTER_KEY_MAP.containsKey(event.keyCode)) {
+            return NON_CHARACTER_KEY_MAP.getValue(event.keyCode)
+        }
+
+        //for all other keys which input characters
+        return event.displayLabel.toString()
+    }
+}
--- app/src/main/java/io/github/sds100/keymapper/TriggerChipGroup.kt
+++ app/src/main/java/io/github/sds100/keymapper/TriggerChipGroup.kt
@@ -3,75 +3,15 @@ package io.github.sds100.keymapper
 import android.content.Context
 import android.util.AttributeSet
 import android.view.KeyEvent
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
 
 /**
- * Created by sds100 on 13/07/2018.
+ * Created by sds100 on 31/07/2018.
  */
 
-/**
- * Controls displaying a trigger as chips
- * @see Chip
- * @see ChipGroup
- */
 class TriggerChipGroup(
         context: Context?,
-        attrs: AttributeSet?,
-        defStyleAttr: Int
-) : ChipGroup(context, attrs, defStyleAttr) {
-
-    companion object {
-
-        /**
-         * Maps keys which aren't single characters like the Control keys to a string representation
-         */
-        private val NON_CHARACTER_KEY_MAP = mapOf(
-                Pair(KeyEvent.KEYCODE_VOLUME_DOWN, "Vol Down"),
-                Pair(KeyEvent.KEYCODE_VOLUME_UP, "Vol Up"),
-
-                Pair(KeyEvent.KEYCODE_CTRL_LEFT, "Ctrl"),
-                Pair(KeyEvent.KEYCODE_CTRL_RIGHT, "Ctrl"),
-
-                Pair(KeyEvent.KEYCODE_SHIFT_LEFT, "Shift"),
-                Pair(KeyEvent.KEYCODE_SHIFT_RIGHT, "Shift"),
-
-                Pair(KeyEvent.KEYCODE_DPAD_LEFT, "Left")
-        )
-    }
-
-    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
-
-    /**
-     * The chips currently being shown in the chip group
-     */
-    private val mChips = mutableListOf<KeyChip>()
-
-    /**
-     * Create a new chip view and show it in the group
-     */
-    fun addChip(event: KeyEvent) {
-        val chip = KeyChip(context, event.keyCode)
-        val text = createTextFromKeyEvent(event)
-
-        chip.text = text
-
-        mChips.add(chip)
-
-        addView(chip)
-    }
-
-    fun removeChip(keyCode: Int) {
-        val chip = mChips.find { it.keyCode == keyCode }
-
-        removeView(chip)
-        mChips.remove(chip)
-    }
-
-    fun removeAllChips() {
-        removeAllViews()
-        mChips.clear()
-    }
+        attrs: AttributeSet?
+) : KeyEventChipGroup(context, attrs) {
 
     /**
      * Show chips for each item in a trigger
@@ -86,22 +26,8 @@ class TriggerChipGroup(
      * @return a [Trigger] whose keys will be the chips in this chip group
      */
     fun createTriggerFromChips(): Trigger {
-        val keys = mChips.map { it.keyCode }
+        val keys = chips.map { it.keyCode }
 
         return Trigger(keys)
-    }
-
-    /**
-     * Create a text representation of a key event. E.g if the control key was pressed,
-     * "Ctrl" will be returned
-     */
-    private fun createTextFromKeyEvent(event: KeyEvent): String {
-
-        if (NON_CHARACTER_KEY_MAP.containsKey(event.keyCode)) {
-            return NON_CHARACTER_KEY_MAP.getValue(event.keyCode)
-        }
-
-        //for all other keys which input characters
-        return event.displayLabel.toString()
     }
 }
