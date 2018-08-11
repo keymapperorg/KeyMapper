5f3aee5efcef5b977db02f4260812c52f13e0dda
sds2001 <seth.d.schroeder@gmail.com>
2018-08-11 11:59:12 +0100

    MainActivity: show key maps in a list

--- app/src/main/java/io/github/sds100/keymapper/Adapters/KeyMapAdapter.kt
+++ app/src/main/java/io/github/sds100/keymapper/Adapters/KeyMapAdapter.kt
@@ -3,9 +3,12 @@ package io.github.sds100.keymapper.Adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.github.sds100.keymapper.Action
 import io.github.sds100.keymapper.KeyMap
 import io.github.sds100.keymapper.R
+import kotlinx.android.synthetic.main.keymap_adapter_item.view.*
 
 /**
  * Created by sds100 on 12/07/2018.
@@ -14,25 +17,39 @@ import io.github.sds100.keymapper.R
 /**
  * Display a list of [KeyMap]s in a RecyclerView
  */
-class KeyMapAdapter(private val keyMapList: List<KeyMap>
-) : RecyclerView.Adapter<KeyMapAdapter.ViewHolder>() {
+class KeymapAdapter(var keyMapList: List<KeyMap>
+) : RecyclerView.Adapter<KeymapAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
 
-        return ViewHolder(inflater.inflate(R.layout.keymap_adapter_item, null))
+        return ViewHolder(inflater.inflate(R.layout.keymap_adapter_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
         holder.itemView.apply {
+            val keyMap = keyMapList[position]
+            val triggerAdapter = TriggerAdapter(keyMap.triggerList, showRemoveButton = false)
 
+            textViewTitle.text = Action.getDescription(context, keyMap.action)
+
+            recyclerViewTriggers.layoutManager = LinearLayoutManager(context)
+            recyclerViewTriggers.adapter = triggerAdapter
+
+            /*if no icon should be shown then hide the ImageView so there isn't whitespace next to
+            the text*/
+            val drawable = Action.getIcon(context, keyMap.action)
+            if (drawable == null) {
+                imageView.setImageDrawable(null)
+                imageView.visibility = View.GONE
+            } else {
+                imageView.setImageDrawable(drawable)
+                imageView.visibility = View.VISIBLE
+            }
         }
     }
 
     override fun getItemCount() = keyMapList.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
--- app/src/main/java/io/github/sds100/keymapper/Adapters/TriggerAdapter.kt
+++ app/src/main/java/io/github/sds100/keymapper/Adapters/TriggerAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import io.github.sds100.keymapper.R
 import io.github.sds100.keymapper.SquareImageButton
 import io.github.sds100.keymapper.Trigger
-import io.github.sds100.keymapper.TriggerChipGroup
+import kotlinx.android.synthetic.main.trigger_adapter_item.view.*
 
 /**
  * Created by sds100 on 16/07/2018.
@@ -16,7 +16,9 @@ import io.github.sds100.keymapper.TriggerChipGroup
 /**
  * Display a list of [Trigger]s as Chips in a RecyclerView
  */
-class TriggerAdapter(val triggerList: MutableList<Trigger> = mutableListOf()
+class TriggerAdapter(
+        val triggerList: MutableList<Trigger> = mutableListOf(),
+        val showRemoveButton: Boolean = true
 ) : RecyclerView.Adapter<TriggerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,8 +28,12 @@ class TriggerAdapter(val triggerList: MutableList<Trigger> = mutableListOf()
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.apply {
+        holder.itemView.apply {
             triggerChipGroup.addChipsFromTrigger(triggerList[position])
+
+            if (!showRemoveButton) {
+                buttonRemove.visibility = View.GONE
+            }
         }
     }
 
@@ -42,8 +48,6 @@ class TriggerAdapter(val triggerList: MutableList<Trigger> = mutableListOf()
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val triggerChipGroup: TriggerChipGroup = itemView.findViewById(R.id.triggerChipGroup)
-
         init {
             itemView.findViewById<SquareImageButton>(R.id.buttonRemove).setOnClickListener {
                 triggerList.removeAt(adapterPosition)
