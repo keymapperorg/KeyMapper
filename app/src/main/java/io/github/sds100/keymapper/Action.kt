6713c2076aabba48d62a928baa163f6e3e5c0d20
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-09-03 19:36:39 +0100

    Action: move some methods to separate ActionUtils

--- app/src/main/java/io/github/sds100/keymapper/Action.kt
+++ app/src/main/java/io/github/sds100/keymapper/Action.kt
@@ -1,17 +1,11 @@
 package io.github.sds100.keymapper
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.drawable.Drawable
-import android.view.KeyEvent
-import androidx.core.content.ContextCompat
-
 /**
  * Created by sds100 on 16/07/2018.
  */
 
 /**
- * @property [data] The information required to perform the action. E.g if the type is [TYPE_APP]
+ * @property [data] The information required to perform the action. E.g if the type is [ActionType.APP]
  * then the data will be the package name of the application
  *
  * Different Types of actions:
@@ -28,87 +22,5 @@ data class Action(
 ) {
     companion object {
         const val EXTRA_ACTION = "extra_action"
-
-        /**
-         * Get a description for an action. E.g if the user chose an app, then the description will
-         * be 'Open <app>'
-         */
-        fun getDescription(ctx: Context, action: Action): String? {
-            return when (action.type) {
-                ActionType.APP -> {
-                    val applicationInfo = ctx.packageManager.getApplicationInfo(
-                            action.data,
-                            PackageManager.GET_META_DATA
-                    )
-
-                    val applicationLabel = ctx.packageManager.getApplicationLabel(applicationInfo)
-
-                    return ctx.getString(R.string.description_open_app, applicationLabel.toString())
-                }
-
-                ActionType.APP_SHORTCUT -> {
-                    val appShortcutList = AppShortcutHelper.getAppShortcuts(ctx.packageManager)
-                    val appShortcut = appShortcutList.find { it.activityInfo.name == action.data }
-
-                    return if (appShortcut == null) {
-                        null
-                    } else {
-                        appShortcut.loadLabel(ctx.packageManager).toString()
-                    }
-                }
-
-                ActionType.SYSTEM_ACTION -> {
-                    //convert the string representation of the enum entry into an enum object
-                    val systemActionEnum = SystemAction.valueOf(action.data)
-                    ctx.getString(SystemActionHelper.getDescription(systemActionEnum))
-                }
-
-                ActionType.KEYCODE -> {
-                    val key = KeyEvent.keyCodeToString(action.data.toInt())
-                    ctx.getString(R.string.description_keycode, key)
-                }
-
-                ActionType.KEY -> {
-                    val keyCode = action.data.toInt()
-                    val key = KeycodeHelper.keycodeToString(keyCode)
-                    ctx.getString(R.string.description_key, key)
-                }
-
-                ActionType.TEXT_BLOCK -> {
-                    val text = action.data
-                    ctx.getString(R.string.description_text_block, text)
-                }
-            }
-        }
-
-        /**
-         * Get the icon for any Action
-         */
-        fun getIcon(ctx: Context, action: Action): Drawable? {
-            return when (action.type) {
-                ActionType.APP -> ctx.packageManager.getApplicationIcon(action.data)
-
-                ActionType.SYSTEM_ACTION -> {
-                    //convert the string representation of the enum entry into an enum object
-                    val systemActionEnum = SystemAction.valueOf(action.data)
-                    val resId = SystemActionHelper.getIconResource(systemActionEnum)
-                    ContextCompat.getDrawable(ctx, resId)
-                }
-
-                ActionType.APP_SHORTCUT -> {
-                    val appShortcutList = AppShortcutHelper.getAppShortcuts(ctx.packageManager)
-                    val appShortcut = appShortcutList.find { it.activityInfo.name == action.data }
-
-                    return if (appShortcut == null) {
-                        null
-                    } else {
-                        appShortcut.loadIcon(ctx.packageManager)
-                    }
-                }
-
-            //return null if no icon should be used
-                else -> null
-            }
-        }
     }
 }
