b86ee60274fa7f3887cbbd33b25c36618331b33f
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-09-05 17:47:11 +0100

    Created ViewModels

---
+++ app/src/main/java/io/github/sds100/keymapper/ViewModels/NewKeyMapViewModel.kt
@@ -0,0 +1,11 @@
+package io.github.sds100.keymapper.ViewModels
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by sds100 on 05/09/2018.
+ */
+
+class NewKeyMapViewModel : ViewModel() {
+    
+}
