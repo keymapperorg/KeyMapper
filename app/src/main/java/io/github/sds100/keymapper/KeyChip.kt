d662ccaba7c3b6567944e65c010807cf888a59e8
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-09-08 18:44:15 +0100

    KeyChip: prevent ripple animation when pressed

--- app/src/main/java/io/github/sds100/keymapper/KeyChip.kt
+++ app/src/main/java/io/github/sds100/keymapper/KeyChip.kt
@@ -1,10 +1,35 @@
 package io.github.sds100.keymapper
 
 import android.content.Context
-import com.google.android.material.chip.Chip
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.google.android.material.chip.ChipDrawable
 
 /**
  * Created by sds100 on 15/07/2018.
  */
 
-class KeyChip(context: Context?, val keyCode: Int) : Chip(context)
+class KeyChip(context: Context?, attrs: AttributeSet?, val keyCode: Int
+) : AppCompatTextView(context, attrs) {
+
+    constructor(context: Context?, keyCode: Int) : this(context, null, keyCode)
+
+    private val mChipDrawable = ChipDrawable.createFromAttributes(
+            context,
+            null,
+            0,
+            R.style.Widget_MaterialComponents_Chip_Action
+    )
+
+    /* Uses a ChipDrawable as the background because it only has to look like a Chip and not behave
+    * like one. Also if it inherits Chip, then it shows a ripple animation when clicked, which is
+    * not what should happen. */
+    init {
+        background = mChipDrawable
+    }
+
+    var text: String = ""
+        set(value) {
+            mChipDrawable.setText(value)
+        }
+}
