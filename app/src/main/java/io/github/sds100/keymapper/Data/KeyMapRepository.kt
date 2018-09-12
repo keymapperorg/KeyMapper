f23121848b92b4f253468d316433e83012ef223b
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-09-12 17:16:56 +0100

    KeyMapRepository: create a list of keymaps for debugging

--- app/src/main/java/io/github/sds100/keymapper/Data/KeyMapRepository.kt
+++ app/src/main/java/io/github/sds100/keymapper/Data/KeyMapRepository.kt
@@ -1,12 +1,15 @@
 package io.github.sds100.keymapper.Data
 
 import android.content.Context
+import android.view.KeyEvent
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import io.github.sds100.keymapper.*
 import io.github.sds100.keymapper.DatabaseAsyncTasks.DeleteKeyMapAsync
 import io.github.sds100.keymapper.DatabaseAsyncTasks.DeleteKeyMapByIdAsync
 import io.github.sds100.keymapper.DatabaseAsyncTasks.InsertKeyMapAsync
 import io.github.sds100.keymapper.DatabaseAsyncTasks.UpdateKeyMapAsync
-import io.github.sds100.keymapper.KeyMap
+import kotlin.coroutines.experimental.buildSequence
 
 /**
  * Created by sds100 on 08/08/2018.
@@ -17,6 +20,8 @@ import io.github.sds100.keymapper.KeyMap
  */
 class KeyMapRepository private constructor(ctx: Context) {
     companion object {
+        private const val DEBUG_LIST_COUNT = 100
+
         private var INSTANCE: KeyMapRepository? = null
 
         fun getInstance(ctx: Context): KeyMapRepository {
@@ -34,6 +39,32 @@ class KeyMapRepository private constructor(ctx: Context) {
 
     init {
         keyMapList = mDb.keyMapDao().getAllKeyMaps()
+
+        if (BuildConfig.DEBUG) {
+            val observer = Observer<List<KeyMap>> { list ->
+                val size = list!!.size
+
+                if (size < DEBUG_LIST_COUNT) {
+                    val sizeDifference = DEBUG_LIST_COUNT - size
+
+                    val testKeyMapList = buildSequence {
+                        for (i in 1..sizeDifference) {
+                            val triggerList = mutableListOf(
+                                    Trigger(listOf(KeyEvent.KEYCODE_VOLUME_UP))
+                            )
+
+                            yield(KeyMap(i.toLong(),
+                                    triggerList,
+                                    Action(ActionType.APP, "io.github.sds100.keymapper")))
+                        }
+                    }.toList()
+
+                    addKeyMap(*testKeyMapList.toTypedArray())
+                }
+            }
+
+            keyMapList.observeForever(observer)
+        }
     }
 
     fun deleteKeyMap(vararg keyMap: KeyMap) {
