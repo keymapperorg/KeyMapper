7e0b00c359e8b7e4bb29ed9ae49c2a60469acf1f
sds100 <seth.d.schroeder@gmail.com>
2018-09-28 18:42:04 +0100

    Created IME Service

---
+++ app/src/main/java/io/github/sds100/keymapper/Services/MyIMEService.kt
@@ -0,0 +1,9 @@
+package io.github.sds100.keymapper.Services
+
+import android.inputmethodservice.InputMethodService
+
+/**
+ * Created by sds100 on 28/09/2018.
+ */
+class MyIMEService : InputMethodService() {
+}
