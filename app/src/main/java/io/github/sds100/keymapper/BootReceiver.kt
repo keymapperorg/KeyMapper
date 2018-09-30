347f2666e65b7672a3c2a523e48213de35d93e9e
sds100 <seth.d.schroeder@gmail.com>
2018-09-30 15:24:26 +0100

    MainActivity: show the notification when app starts

--- app/src/main/java/io/github/sds100/keymapper/BootReceiver.kt
+++ app/src/main/java/io/github/sds100/keymapper/BootReceiver.kt
@@ -3,7 +3,6 @@ package io.github.sds100.keymapper
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.core.app.NotificationManagerCompat
 import io.github.sds100.keymapper.Utils.NotificationUtils
 
 /**
@@ -13,11 +12,7 @@ class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent!!.action == Intent.ACTION_BOOT_COMPLETED) {
-            val notification = NotificationUtils.buildIMEPickerNotification(context!!)
-
-            with(NotificationManagerCompat.from(context)) {
-                notify(NotificationUtils.PERSISTENT_NOTIFICATION_ID, notification)
-            }
+            NotificationUtils.showIMEPickerNotification(context!!)
         }
     }
 }
