7fae38f2e4d63990cf0d5ed1927b72f7fe7f750e
sds100 <seth.d.schroeder@gmail.com>
2018-10-01 20:18:06 +0100

    Create SettingsActivity

--- app/src/main/java/io/github/sds100/keymapper/Activities/SettingsActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/Activities/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package io.github.sds100.keymapper.Activities
 
+import androidx.appcompat.app.AppCompatActivity
+
 /**
  * Created by sds100 on 01/10/2018.
  */
-class SettingsActivity {
+
+class SettingsActivity : AppCompatActivity() {
+
 }
