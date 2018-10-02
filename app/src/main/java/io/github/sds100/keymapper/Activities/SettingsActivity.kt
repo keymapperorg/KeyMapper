560093017fc6d950e2f0651ff51228368e8ae1b9
sds100 <seth.d.schroeder@gmail.com>
2018-10-02 17:49:47 +0100

    SettingsActivity: disable "Show notification on boot" setting if the notification is turned off

--- app/src/main/java/io/github/sds100/keymapper/Activities/SettingsActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/Activities/SettingsActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.transaction
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import io.github.sds100.keymapper.R
 
 /**
@@ -28,8 +30,8 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item!!.itemId) {
-            /*when the back button in the toolbar is pressed, call onBackPressed so it acts like the
-            hardware back button */
+            /* when the back button in the toolbar is pressed, call onBackPressed so it acts like
+            the hardware back button */
             android.R.id.home -> {
                 onBackPressed()
                 true
@@ -38,9 +40,37 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+
+        private val mShowNotificationPreference by lazy {
+            findPreference(getString(R.string.key_pref_show_notification)) as SwitchPreference
+        }
+
+        private val mShowNotificationOnBootPreference by lazy {
+            findPreference(getString(R.string.key_pref_show_notification_on_boot))
+                    as SwitchPreference
+        }
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             addPreferencesFromResource(R.xml.preferences)
+
+            mShowNotificationPreference.onPreferenceChangeListener = this
+
+            /*only allow the user to toggle whether the notification shows on boot if they want
+            * to see the notification at all. */
+            mShowNotificationOnBootPreference.isEnabled = mShowNotificationPreference.isChecked
+        }
+
+        override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+            when (preference) {
+                mShowNotificationPreference -> {
+                    /* if the user doesn't want to see the notification, don't allow them
+                     * to toggle whether it is shown on boot on and off */
+                    mShowNotificationOnBootPreference.isEnabled = newValue as Boolean
+                }
+            }
+
+            return true
         }
     }
 }
