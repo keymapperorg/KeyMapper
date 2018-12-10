11de3877b63913435dcb6b5c4fdf32092c90fac8
sds100 <seth.d.schroeder@gmail.com>
2018-12-10 18:42:32 +0000

    opening the About activity now works

---
+++ app/src/main/java/io/github/sds100/keymapper/Activities/AboutFragment.kt
@@ -0,0 +1,14 @@
+package io.github.sds100.keymapper.Activities
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+/**
+ * Created by sds100 on 10/12/2018.
+ */
+
+class AboutFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+    }
+
+}
