2c04785c01e5d3998528e80fb3ed5426910622f6
sds100 <seth.d.schroeder@gmail.com>
2018-12-19 17:16:07 +0000

    Created HelpActivity

---
+++ app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
@@ -0,0 +1,11 @@
+package io.github.sds100.keymapper.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by sds100 on 19/12/2018.
+ */
+
+class HelpActivity : AppCompatActivity() {
+
+}
