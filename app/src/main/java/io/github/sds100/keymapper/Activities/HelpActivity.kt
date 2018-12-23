7076627b868391a94e1700420e1156ca29e2e566
SDS-T430\sethd <seth.d.schroeder@gmail.com>
2018-12-23 12:31:27 +0000

    created HelpActivity layout

--- app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
@@ -1,6 +1,9 @@
 package io.github.sds100.keymapper.Activities
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.sds100.keymapper.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by sds100 on 19/12/2018.
@@ -8,4 +11,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class HelpActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+        setSupportActionBar(toolbar)
+    }
 }
