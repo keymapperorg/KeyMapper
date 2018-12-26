c6f5f54e1cc64cf254b41ba7a37a1d2c9aab4ca9
sds100 <seth.d.schroeder@gmail.com>
2018-12-26 17:34:36 +0000

    OpenIMEPickerBroadcastReceiver: move code to show input method picker via root to a method

--- app/src/main/java/io/github/sds100/keymapper/BroadcastReceiver/OpenIMEPickerBroadcastReceiver.kt
+++ app/src/main/java/io/github/sds100/keymapper/BroadcastReceiver/OpenIMEPickerBroadcastReceiver.kt
@@ -52,22 +52,23 @@ class OpenIMEPickerBroadcastReceiver : BroadcastReceiver() {
                 }
             }
 
-            /* Android 8.1 and higher don't seem to allow you to open the input method picker dialog
-                 * from outside the app :( but it can be achieved by sending a broadcast with a
-                 * system process id (requires root access) */
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O_MR1) {
-                showInputMethodPickerDialog(context!!)
-            } else {
-                val command =
-                        "am broadcast -a com.android.server.InputMethodManagerService.SHOW_INPUT_METHOD_PICKER"
-                RootUtils.executeRootCommand(command)
-            }
+            showInputMethodPickerDialog(context!!)
         }
     }
 
     private fun showInputMethodPickerDialog(ctx: Context) {
-        val imeManager = ctx.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        /* Android 8.1 and higher don't seem to allow you to open the input method picker dialog
+             * from outside the app :( but it can be achieved by sending a broadcast with a
+             * system process id (requires root access) */
 
-        imeManager.showInputMethodPicker()
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O_MR1) {
+            val imeManager = ctx.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+            imeManager.showInputMethodPicker()
+        } else {
+            val command =
+                    "am broadcast -a com.android.server.InputMethodManagerService.SHOW_INPUT_METHOD_PICKER"
+            RootUtils.executeRootCommand(command)
+        }
     }
 }
