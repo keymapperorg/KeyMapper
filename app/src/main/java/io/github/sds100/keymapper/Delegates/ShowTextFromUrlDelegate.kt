8d2873a83e5320aff58689269d7d086e7fab7ca6
sds100 <seth.d.schroeder@gmail.com>
2018-12-26 17:48:42 +0000

    Delegate how the markdown file is retrieved

---
+++ app/src/main/java/io/github/sds100/keymapper/Delegates/ShowTextFromUrlDelegate.kt
@@ -0,0 +1,53 @@
+package io.github.sds100.keymapper.Delegates
+
+import android.content.Context
+import io.github.sds100.keymapper.R
+import io.github.sds100.keymapper.Utils.FileUtils
+import io.github.sds100.keymapper.Utils.NetworkUtils
+import io.github.sds100.keymapper.Views.ProgressDialog
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.File
+import java.net.URL
+
+/**
+ * Created by sds100 on 26/12/2018.
+ */
+
+/**
+ * Downloads a specified file from a url and allows the text from that file to be shown however you want.
+ * It saves the file locally so it can be viewed while offline.
+ */
+interface ShowTextFromUrlDelegate {
+
+    fun showTextFromUrl(ctx: Context, fileUrl: String, onShowText: (text: String) -> Unit) {
+        //show a progress dialog
+        val progressDialog = ProgressDialog(ctx, R.string.dialog_message_downloading)
+
+        progressDialog.show()
+
+        doAsync {
+            val text = getText(ctx, fileUrl)
+
+            uiThread {
+                //dismiss the progress dialog once the changelog has been received
+                progressDialog.dismiss()
+                onShowText(text)
+            }
+        }
+    }
+
+    private fun getText(ctx: Context, fileUrl: String): String {
+        val path = FileUtils.getPathToFileInAppData(ctx, extractFileName(fileUrl))
+        NetworkUtils.downloadFile(ctx, fileUrl, path)
+
+        return FileUtils.getTextFromFile(ctx, extractFileName(fileUrl))
+    }
+
+    /**
+     * Extracts the file name from the url
+     */
+    private fun extractFileName(fileUrl: String): String {
+        return File(URL(fileUrl).path.toString()).name
+    }
+}
