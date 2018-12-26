5c1a5c6873f53ee58cc3e543216ad5d62e485090
sds100 <seth.d.schroeder@gmail.com>
2018-12-26 17:55:20 +0000

    HelpActivity: the Help page in the github project wiki is now shown.

--- app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
@@ -3,14 +3,17 @@ package io.github.sds100.keymapper.Activities
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import io.github.sds100.keymapper.Delegates.ShowTextFromUrlDelegate
 import io.github.sds100.keymapper.R
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_help.*
 
 /**
  * Created by sds100 on 19/12/2018.
  */
 
-class HelpActivity : AppCompatActivity() {
+class HelpActivity : AppCompatActivity(), ShowTextFromUrlDelegate {
+
+    private val mHelpMarkdownUrl by lazy { getString(R.string.url_help) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,10 @@ class HelpActivity : AppCompatActivity() {
 
         //show the back button in the toolbar
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        showTextFromUrl(this, mHelpMarkdownUrl) { text ->
+            markdownView.setMarkDownText(text)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
