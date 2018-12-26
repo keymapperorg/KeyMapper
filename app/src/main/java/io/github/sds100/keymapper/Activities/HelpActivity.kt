fdd7ed793de1adf225a1ab488e940f59efd246d1
sds100 <seth.d.schroeder@gmail.com>
2018-12-26 17:33:41 +0000

    HelpActivity: add back button

--- app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/Activities/HelpActivity.kt
@@ -1,6 +1,7 @@
 package io.github.sds100.keymapper.Activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import io.github.sds100.keymapper.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,5 +16,20 @@ class HelpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
         setSupportActionBar(toolbar)
+
+        //show the back button in the toolbar
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            /*when the back button in the toolbar is pressed, call onBackPressed so it acts like the
+            hardware back button */
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
