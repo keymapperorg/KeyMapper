b8f4cd1b9c155f4d2552e49187001b1169e95c4c
sds100 <seth.d.schroeder@gmail.com>
2019-03-09 17:27:39 +0000

    KeyMap: enable SHOW_VOLUME_UI flag by default

--- app/src/main/java/io/github/sds100/keymapper/KeyMap.kt
+++ app/src/main/java/io/github/sds100/keymapper/KeyMap.kt
@@ -5,21 +5,21 @@ import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import io.github.sds100.keymapper.data.KeyMapDao
+import io.github.sds100.keymapper.util.FlagUtils
+import io.github.sds100.keymapper.util.addFlag
+import io.github.sds100.keymapper.util.containsFlag
 
 /**
  * Created by sds100 on 12/07/2018.
  */
 
 @Entity(tableName = KeyMapDao.TABLE_NAME)
-data class KeyMap(
+class KeyMap(
         @PrimaryKey(autoGenerate = true)
         val id: Long,
 
         @ColumnInfo(name = KeyMapDao.KEY_TRIGGER_LIST)
         val triggerList: MutableList<Trigger> = mutableListOf(),
-
-        @Embedded
-        var action: Action? = null,
 
         @ColumnInfo(name = KeyMapDao.KEY_FLAGS)
         /**
@@ -30,6 +30,19 @@ data class KeyMap(
         @ColumnInfo(name = KeyMapDao.KEY_ENABLED)
         var isEnabled: Boolean = true
 ) {
+    @Embedded
+    var action: Action? = null
+        set(value) {
+            if (value.isVolumeAction) {
+                flags = addFlag(flags, FlagUtils.FLAG_SHOW_VOLUME_UI)
+            }
+
+            field = value
+        }
+
+    val isLongPress
+        get() = containsFlag(flags, FlagUtils.FLAG_LONG_PRESS)
+
     override fun hashCode() = id.toInt()
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -40,5 +53,15 @@ data class KeyMap(
         if (id != other.id) return false
 
         return true
+    }
+
+    fun clone() = KeyMap(id, triggerList, flags, isEnabled).apply {
+        action = this@KeyMap.action
+    }
+
+    fun containsTrigger(keyCodes: List<Int>): Boolean {
+        return triggerList.any { trigger ->
+            trigger.keys.toTypedArray().contentEquals(keyCodes.toTypedArray())
+        }
     }
 }
