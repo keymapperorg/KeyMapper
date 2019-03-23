2d81cbb7310e6ac0398c34d4e696a16afe5d8d76
sds100 <seth.d.schroeder@gmail.com>
2019-03-23 13:05:55 +0000

    HomeActivity: removed old menu

--- app/src/main/java/io/github/sds100/keymapper/activity/HomeActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/activity/HomeActivity.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.provider.Settings
 import android.text.SpannableStringBuilder
 import android.text.style.RelativeSizeSpan
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.annotation.ColorRes
@@ -221,39 +220,6 @@ class HomeActivity : AppCompatActivity(), SelectionCallback,
         super.onDestroy()
 
         unregisterReceiver(mBroadcastReceiver)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
-                true
-            }
-
-            R.id.action_about -> {
-                val intent = Intent(this, AboutActivity::class.java)
-                startActivity(intent)
-                true
-            }
-
-            R.id.action_help -> {
-                val intent = Intent(this, HelpActivity::class.java)
-                startActivity(intent)
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onSelectionEvent(id: Long?, event: SelectionEvent) {
