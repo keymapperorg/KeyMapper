9831b1249a2e0cd019b5ae294cbd6ec6960ba792
sds100 <seth.d.schroeder@gmail.com>
2019-05-22 20:33:24 +0100

    ScreenRotationUtils: use extension function to access system settings

--- app/src/main/java/io/github/sds100/keymapper/util/ScreenRotationUtils.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/ScreenRotationUtils.kt
@@ -14,7 +14,7 @@ object ScreenRotationUtils {
 
         //auto rotate must be disabled for this to work
         disableAutoRotate(ctx)
-        Settings.System.putInt(ctx.contentResolver, Settings.System.USER_ROTATION, Surface.ROTATION_0)
+        ctx.putSystemSetting(Settings.System.USER_ROTATION, Surface.ROTATION_0)
     }
 
     fun forceLandscapeMode(ctx: Context) {
@@ -22,7 +22,7 @@ object ScreenRotationUtils {
 
         //auto rotate must be disabled for this to work
         disableAutoRotate(ctx)
-        Settings.System.putInt(ctx.contentResolver, Settings.System.USER_ROTATION, Surface.ROTATION_90)
+        ctx.putSystemSetting(Settings.System.USER_ROTATION, Surface.ROTATION_90)
     }
 
     fun switchOrientation(ctx: Context) {
@@ -38,22 +38,19 @@ object ScreenRotationUtils {
         //don't attempt to enable auto rotate if they app doesn't have permission
         if (!ctx.haveWriteSettingsPermission) return
 
-        Settings.System.putInt(ctx.contentResolver,
-                Settings.System.ACCELEROMETER_ROTATION, 1)
+        ctx.putSystemSetting(Settings.System.ACCELEROMETER_ROTATION, 1)
     }
 
     fun disableAutoRotate(ctx: Context) {
         //don't attempt to enable auto rotate if they app doesn't have permission
         if (!ctx.haveWriteSettingsPermission) return
 
-        Settings.System.putInt(ctx.contentResolver,
-                Settings.System.ACCELEROMETER_ROTATION, 0)
+        ctx.putSystemSetting(Settings.System.ACCELEROMETER_ROTATION, 0)
     }
 
     fun toggleAutoRotate(ctx: Context) {
 
-        val autoRotateState = Settings.System.getInt(ctx.contentResolver,
-                Settings.System.ACCELEROMETER_ROTATION)
+        val autoRotateState = ctx.getSystemSetting<Int>(Settings.System.ACCELEROMETER_ROTATION)
 
         if (autoRotateState == 0) {
             enableAutoRotate(ctx)
@@ -66,7 +63,7 @@ object ScreenRotationUtils {
      * @return If the rotation setting can't be found, it returns null
      */
     fun isPortrait(ctx: Context): Boolean? {
-        val setting = Settings.System.getInt(ctx.contentResolver, Settings.System.USER_ROTATION)
+        val setting = ctx.getSystemSetting<Int>(Settings.System.USER_ROTATION)
         return setting == Surface.ROTATION_0 || setting == Surface.ROTATION_180
     }
 
@@ -74,7 +71,7 @@ object ScreenRotationUtils {
      * @return If the rotation setting can't be found, it returns null
      */
     fun isLandscape(ctx: Context): Boolean? {
-        val setting = Settings.System.getInt(ctx.contentResolver, Settings.System.USER_ROTATION)
+        val setting = ctx.getSystemSetting<Int>(Settings.System.USER_ROTATION)
         return setting == Surface.ROTATION_90 || setting == Surface.ROTATION_270
     }
 }
