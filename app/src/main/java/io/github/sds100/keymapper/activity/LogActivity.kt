e0501fca6001b6752b4211712606e28cca3205e7
sds100 <seth.d.schroeder@gmail.com>
2019-05-23 20:59:54 +0100

    LogActivity: prompt the user to describe the issue in the email

--- app/src/main/java/io/github/sds100/keymapper/activity/LogActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/activity/LogActivity.kt
@@ -7,6 +7,8 @@ import io.github.sds100.keymapper.R
 import io.github.sds100.keymapper.fragment.LogFragment
 import io.github.sds100.keymapper.util.FeedbackUtils
 import io.github.sds100.keymapper.util.Logger
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.okButton
 
 /**
  * Created by sds100 on 11/05/2019.
@@ -15,7 +17,6 @@ import io.github.sds100.keymapper.util.Logger
 class LogActivity : SimpleFragmentActivity() {
     override val fragmentToShow: Fragment
         get() = LogFragment()
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_log, menu)
@@ -28,7 +29,16 @@ class LogActivity : SimpleFragmentActivity() {
             R.id.action_delete_log -> Logger.delete(this)
 
             R.id.action_send_log -> {
-                FeedbackUtils.sendFeedback(this, "\n\nHere is a log:\n${Logger.read(this)}")
+                alert {
+                    messageResource = R.string.dialog_message_describe_your_issue_in_the_email
+                    okButton {
+                        FeedbackUtils.sendFeedback(
+                                this@LogActivity,
+                                "Can you please describe your issue here:\n\n" +
+                                        "Here is a log:\n${Logger.read(this@LogActivity)}"
+                        )
+                    }
+                }.show()
             }
         }
 
