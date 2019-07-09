ffe708f7ce8d1da482396070c0580ea815b615a9
sds100 <seth.d.schroeder@gmail.com>
2019-07-09 18:20:12 +0100

    IntroActivity: added "note from developer" slide

--- app/src/main/java/io/github/sds100/keymapper/activity/IntroActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/activity/IntroActivity.kt
@@ -2,6 +2,8 @@ package io.github.sds100.keymapper.activity
 
 import android.os.Bundle
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+import io.github.sds100.keymapper.R
 
 /**
  * Created by sds100 on 07/07/2019.
@@ -12,5 +14,13 @@ class IntroActivity : IntroActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        addSlide(SimpleSlide.Builder().apply {
+            title(R.string.showcase_note_from_the_developer)
+            description(R.string.showcase_note_from_the_developer_message)
+            background(R.color.red)
+            backgroundDark(R.color.redDark)
+            scrollable(false)
+            canGoBackward(true)
+        }.build())
     }
 }
