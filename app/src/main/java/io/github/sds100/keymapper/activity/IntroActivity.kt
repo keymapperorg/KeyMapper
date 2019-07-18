8241b25d7ce1f72b5b84df362cb80d172b9bad5d
sds100 <seth.d.schroeder@gmail.com>
2019-07-18 15:46:41 +0100

    Intro: show page to disable battery optimisation.

--- app/src/main/java/io/github/sds100/keymapper/activity/IntroActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/activity/IntroActivity.kt
@@ -1,15 +1,21 @@
 package io.github.sds100.keymapper.activity
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+import io.github.sds100.keymapper.Constants
 import io.github.sds100.keymapper.R
 
 /**
  * Created by sds100 on 07/07/2019.
  */
 
-class IntroActivity : IntroActivity(){
+class IntroActivity : IntroActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +27,30 @@ class IntroActivity : IntroActivity(){
             backgroundDark(R.color.redDark)
             scrollable(false)
             canGoBackward(true)
-        }.build())
+            isSkipEnabled = false
+    }.build())
+
+        val powerManager = (getSystemService(Context.POWER_SERVICE)) as PowerManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !powerManager.isIgnoringBatteryOptimizations(Constants.PACKAGE_NAME)) {
+
+            addSlide(SimpleSlide.Builder().apply {
+                title(R.string.showcase_disable_battery_optimisation)
+                description(R.string.showcase_disable_battery_optimisation_message)
+                background(R.color.blue)
+                backgroundDark(R.color.blueDark)
+                image(R.drawable.ic_battery_std_white_64dp)
+                scrollable(false)
+                canGoBackward(true)
+                isSkipEnabled = false
+
+                buttonCtaLabel(R.string.showcase_disable_battery_optimisation_button)
+                buttonCtaClickListener {
+                    val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+                    startActivity(intent)
+                }
+            }.build())
+        }
     }
 }
