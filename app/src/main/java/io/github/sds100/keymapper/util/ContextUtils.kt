4bf710b4abcd8fa1fdea120fa5ade9078cf1f115
sds100 <seth.d.schroeder@gmail.com>
2019-08-12 18:51:03 -0500

    ContextUtils: add method to get and write to secure setttings

--- app/src/main/java/io/github/sds100/keymapper/util/ContextUtils.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/ContextUtils.kt
@@ -1,5 +1,6 @@
 package io.github.sds100.keymapper.util
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
@@ -143,6 +144,33 @@ inline fun <reified T> Context.getSystemSetting(name: String): T? {
     }
 }
 
+/**
+ * @return If the setting can't be found, it returns null
+ */
+inline fun <reified T> Context.getSecureSetting(name: String): T? {
+    return try {
+        when (T::class) {
+
+            Int::class -> Settings.Secure.getInt(contentResolver, name) as T?
+            String::class -> Settings.Secure.getString(contentResolver, name) as T?
+            Float::class -> Settings.Secure.getFloat(contentResolver, name) as T?
+            Long::class -> Settings.Secure.getLong(contentResolver, name) as T?
+
+            else -> {
+                throw Exception("Setting type ${T::class} is not supported")
+            }
+        }
+    } catch (e: Settings.SettingNotFoundException) {
+        Logger.write(
+            this,
+            isError = true,
+            title = "Exception",
+            message = "SettingNotFoundException: $name in ContentUtils")
+        null
+    }
+}
+
+@RequiresPermission(Manifest.permission.WRITE_SETTINGS)
 inline fun <reified T> Context.putSystemSetting(name: String, value: T) {
 
     when (T::class) {
@@ -151,6 +179,21 @@ inline fun <reified T> Context.putSystemSetting(name: String, value: T) {
         String::class -> Settings.System.putString(contentResolver, name, value as String)
         Float::class -> Settings.System.putFloat(contentResolver, name, value as Float)
         Long::class -> Settings.System.putLong(contentResolver, name, value as Long)
+
+        else -> {
+            throw Exception("Setting type ${T::class} is not supported")
+        }
+    }
+}
+
+@RequiresPermission(Manifest.permission.WRITE_SECURE_SETTINGS)
+inline fun <reified T> Context.putSecureSetting(name: String, value: T) {
+
+    when (T::class) {
+        Int::class -> Settings.Secure.putInt(contentResolver, name, value as Int)
+        String::class -> Settings.Secure.putString(contentResolver, name, value as String)
+        Float::class -> Settings.Secure.putFloat(contentResolver, name, value as Float)
+        Long::class -> Settings.Secure.putLong(contentResolver, name, value as Long)
 
         else -> {
             throw Exception("Setting type ${T::class} is not supported")
