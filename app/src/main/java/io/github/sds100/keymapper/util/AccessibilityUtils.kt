9178369bb90d53dbae8f93ed8f9d7ecaf8c64183
sds100 <seth.d.schroeder@gmail.com>
2019-12-22 23:13:30 +0000

    fix (AccessibilityUtils): open the accessibility settings such that pressing the back button goes straight back to the app #274
    resolves #274.

--- app/src/main/java/io/github/sds100/keymapper/util/AccessibilityUtils.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/AccessibilityUtils.kt
@@ -65,10 +65,13 @@ object AccessibilityUtils {
         }
     }
 
-    fun openAccessibilitySettings(ctx: Context) {
+    private fun openAccessibilitySettings(ctx: Context) {
         try {
             val settingsIntent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-            settingsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
 
             ctx.startActivity(settingsIntent)
         } catch (e: ActivityNotFoundException) {
