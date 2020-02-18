e6626ce05d8fc74e40ed55624bd950ff46ad214c
sds100 <developer.sds100@gmail.com>
2020-02-18 23:06:11 +0100

    chore: removed all kotlin files and layouts so the app can be started from scratch
    Many of them will be added back over time.

--- app/src/main/java/io/github/sds100/keymapper/interfaces/OnLogChangedListener.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sds100.keymapper.interfaces
-
-/**
- * Created by sds100 on 11/05/2019.
- */
-
-interface OnLogChangedListener {
-    fun onLogChange()
-}
--- app/src/main/java/io/github/sds100/keymapper/util/BooleanUtils.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sds100.keymapper.util
-
-/**
- * Created by sds100 on 22/05/2019.
- */
-
-fun Boolean?.isNotNullAndTrue(): Boolean {
-    return this != null && this == true
-}
