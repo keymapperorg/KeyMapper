82ab11053dd051c13293f93699bb30a173e47221
sds100 <developer.sds100@gmail.com>
2020-05-20 14:54:19 +0100

    ActionsFragment: remove unnecessary line

--- app/src/main/java/io/github/sds100/keymapper/ui/fragment/ActionsFragment.kt
+++ app/src/main/java/io/github/sds100/keymapper/ui/fragment/ActionsFragment.kt
@@ -91,8 +91,6 @@ class ActionsFragment : Fragment() {
                                 mViewModel.onActionModelClick(it)
                             }
                         }
-
-                        onClick(onClickListener)
                     }
                 }
             }
