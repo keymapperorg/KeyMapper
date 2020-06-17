379e99b0d27f2b4bce43bfc6a2f0c1042a9f40d0
sds100 <developer.sds100@gmail.com>
2020-06-17 15:58:10 +0100

    #37 created methods to write and read to a selected file

--- app/src/main/java/io/github/sds100/keymapper/util/FileUtils.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/FileUtils.kt
@@ -1,6 +1,10 @@
 package io.github.sds100.keymapper.util
 
 import android.content.Context
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import java.io.FileOutputStream
 
 /**
@@ -8,6 +12,8 @@ import java.io.FileOutputStream
  */
 
 object FileUtils {
+
+    const val MIME_TYPE_JSON = "application/json"
 
     fun getTextFromAppFiles(ctx: Context, fileName: String): String {
         ctx.openFileInput(fileName).bufferedReader().use {
@@ -24,4 +30,34 @@ object FileUtils {
     }
 
     fun getPathToFileInAppData(ctx: Context, fileName: String) = "${ctx.filesDir.path}/$fileName"
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun readFileContents(activity: AppCompatActivity,
+                         mimeType: String,
+                         onOpen: (contents: String) -> Unit) {
+
+        activity.registerForActivityResult(ActivityResultContracts.GetContent()) {
+            it ?: return@registerForActivityResult
+
+            val inputStream = activity.contentResolver.openInputStream(it)
+
+            val contents = inputStream?.bufferedReader().use { reader -> reader?.readText() }
+                ?: return@registerForActivityResult
+
+            onOpen(contents)
+        }.launch(mimeType)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun writeFileContents(activity: AppCompatActivity, fileName: String, contents: String) {
+        activity.registerForActivityResult(ActivityResultContracts.CreateDocument()) {
+            it ?: return@registerForActivityResult
+
+            val outputStream = activity.contentResolver.openOutputStream(it)
+
+            outputStream?.bufferedWriter().use { writer ->
+                writer?.write(contents)
+            }
+        }.launch(fileName)
+    }
 }
