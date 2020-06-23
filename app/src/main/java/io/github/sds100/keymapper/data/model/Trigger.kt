729eb60d3f97aa0f4fafd18c794babd23a2eaf49
sds100 <developer.sds100@gmail.com>
2020-06-23 12:57:10 +0100

    #373 Trigger.kt: added more SerializedName annotations

--- app/src/main/java/io/github/sds100/keymapper/data/model/Trigger.kt
+++ app/src/main/java/io/github/sds100/keymapper/data/model/Trigger.kt
@@ -1,9 +1,7 @@
 package io.github.sds100.keymapper.data.model
 
-import android.view.KeyEvent
 import androidx.annotation.IntDef
 import com.google.gson.annotations.SerializedName
-import io.github.sds100.keymapper.util.isExternalCompat
 import io.github.sds100.keymapper.util.result.ExtraNotFound
 import io.github.sds100.keymapper.util.result.Result
 import io.github.sds100.keymapper.util.result.Success
@@ -26,6 +24,7 @@ class Trigger(
         //DON'T CHANGE THESE. Used for JSON serialization and parsing.
         const val NAME_KEYS = "keys"
         const val NAME_EXTRAS = "extras"
+        const val NAME_MODE = "mode"
 
         const val PARALLEL = 0
         const val SEQUENCE = 1
@@ -39,28 +38,29 @@ class Trigger(
     }
 
     @Mode
+    @SerializedName(NAME_MODE)
     var mode: Int = DEFAULT_TRIGGER_MODE
 
     class Key(
+        @SerializedName(NAME_KEYCODE)
         val keyCode: Int,
+        @SerializedName(NAME_DEVICE_ID)
         var deviceId: String = DEVICE_ID_THIS_DEVICE,
-        @ClickType var clickType: Int = SHORT_PRESS
+
+        @ClickType
+        @SerializedName(NAME_CLICK_TYPE)
+        var clickType: Int = SHORT_PRESS
     ) {
 
         companion object {
+            //DON'T CHANGE THESE. Used for JSON serialization and parsing.
+            const val NAME_KEYCODE = "keycode"
+            const val NAME_DEVICE_ID = "deviceId"
+            const val NAME_CLICK_TYPE = "clickType"
+
             //IDS! DON'T CHANGE
             const val DEVICE_ID_THIS_DEVICE = "io.github.sds100.keymapper.THIS_DEVICE"
             const val DEVICE_ID_ANY_DEVICE = "io.github.sds100.keymapper.ANY_DEVICE"
-
-            fun fromKeyEvent(keyEvent: KeyEvent): Key {
-                val deviceId = if (keyEvent.device.isExternalCompat) {
-                    keyEvent.device.descriptor
-                } else {
-                    DEVICE_ID_THIS_DEVICE
-                }
-
-                return Key(keyEvent.keyCode, deviceId)
-            }
         }
 
         val uniqueId: String
