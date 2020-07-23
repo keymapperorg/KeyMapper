08ec6e1d18031934fb2fb1cc232be0be7f26c9ea
Seth <developer.sds100@gmail.com>
2020-07-23 23:24:08 +0100

    create Key Mapper Input Method permission.
    This will ensure that only apps with the user's permission can input Key Mapper actions.

--- app/src/main/java/io/github/sds100/keymapper/Constants.kt
+++ app/src/main/java/io/github/sds100/keymapper/Constants.kt
@@ -9,7 +9,11 @@ object Constants {
     const val MIN_API = Build.VERSION_CODES.JELLY_BEAN_MR1
     const val MAX_API = Build.VERSION_CODES.Q
     const val PACKAGE_NAME = BuildConfig.APPLICATION_ID
-    const val PERMISSION_ROOT = "$PACKAGE_NAME.ROOT"
+    const val PERMISSION_ROOT = "io.github.sds100.keymapper.ROOT"
+    const val PERMISSION_KEY_MAPPER_INPUT_METHOD = "io.github.sds100.keymapper.KEY_MAPPER_INPUT_METHOD"
     const val VERSION = BuildConfig.VERSION_NAME
     const val VERSION_CODE = BuildConfig.VERSION_CODE
+
+    const val INPUT_METHOD_ACTION_INPUT_DOWN_UP = "io.github.sds100.keymapper.inputmethod.INPUT_DOWN_UP"
+    const val INPUT_METHOD_EXTRA_KEYCODE = "io.github.sds100.keymapper.inputmethod.KEYCODE"
 }
