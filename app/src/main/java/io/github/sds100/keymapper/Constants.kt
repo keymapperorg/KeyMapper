08f1ca8df95b80f4b20a326cbca4b54abcdfc139
Seth <developer.sds100@gmail.com>
2020-08-01 19:21:18 +0100

    Constants.kt: remove unused constants

--- app/src/main/java/io/github/sds100/keymapper/Constants.kt
+++ app/src/main/java/io/github/sds100/keymapper/Constants.kt
@@ -12,7 +12,4 @@ object Constants {
     const val PERMISSION_ROOT = "io.github.sds100.keymapper.ROOT"
     const val VERSION = BuildConfig.VERSION_NAME
     const val VERSION_CODE = BuildConfig.VERSION_CODE
-
-    const val INPUT_METHOD_ACTION_INPUT_DOWN_UP = "io.github.sds100.keymapper.inputmethod.INPUT_DOWN_UP"
-    const val INPUT_METHOD_EXTRA_KEYCODE = "io.github.sds100.keymapper.inputmethod.KEYCODE"
 }
