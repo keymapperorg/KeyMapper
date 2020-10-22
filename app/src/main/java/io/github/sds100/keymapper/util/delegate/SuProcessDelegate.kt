bcd3ccb0e018c8836061701b2bfa0e9c24c27aa9
sds100 <developer.sds100@gmail.com>
2020-10-22 22:19:51 +0100

    fix (ActionPerformerDelegate) app would crash on Marshmallow and older because a lifecycle owner implemented methods that weren't in the framework.
    Delegated the management of the su process to a separate class as a lifecycle owner instead of making ActionPerformerDelegate.kt the lifecycle owner.

---
+++ app/src/main/java/io/github/sds100/keymapper/util/delegate/SuProcessDelegate.kt
@@ -0,0 +1,37 @@
+package io.github.sds100.keymapper.util.delegate
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import io.github.sds100.keymapper.util.RootUtils
+import timber.log.Timber
+import java.io.IOException
+
+/**
+ * Created by sds100 on 22/10/20.
+ */
+class SuProcessDelegate : LifecycleObserver {
+
+    var process: Process? = null
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    private fun onCreate() {
+        createSuProcess()
+
+        Timber.d("oncreate")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun stopSuProcess() {
+        process?.destroy()
+        Timber.d("ondestroy")
+    }
+
+    fun createSuProcess() {
+        try {
+            process = RootUtils.getSuProcess()
+        } catch (e: IOException) {
+            Timber.e(e)
+        }
+    }
+}
