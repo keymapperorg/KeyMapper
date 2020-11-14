eb5b0236ff2cb2bfde4591660d742fc7a92992e9
sds100 <developer.sds100@gmail.com>
2020-11-14 15:52:44 +0000

    #508 (NotificationReceiver) suppress redundant overrides

--- app/src/main/java/io/github/sds100/keymapper/service/NotificationReceiver.kt
+++ app/src/main/java/io/github/sds100/keymapper/service/NotificationReceiver.kt
@@ -1,11 +1,26 @@
 package io.github.sds100.keymapper.service
 
+import android.content.Intent
 import android.os.Build
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
 import androidx.annotation.RequiresApi
 
 /**
  * Created by sds100 on 14/11/20.
  */
+@Suppress("RedundantOverride")
 @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-class NotificationReceiver : NotificationListenerService()
+class NotificationReceiver : NotificationListenerService() {
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
