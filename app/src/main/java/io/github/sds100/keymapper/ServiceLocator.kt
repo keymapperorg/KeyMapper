ab791686165b824817275525d0b6afe70fbb65a7
Seth <developer.sds100@gmail.com>
2021-01-08 19:36:47 +0000

    #490 database migration

--- app/src/main/java/io/github/sds100/keymapper/ServiceLocator.kt
+++ app/src/main/java/io/github/sds100/keymapper/ServiceLocator.kt
@@ -172,7 +172,8 @@ object ServiceLocator {
             AppDatabase.MIGRATION_4_5,
             AppDatabase.MIGRATION_5_6,
             AppDatabase.MIGRATION_6_7,
-            AppDatabase.MIGRATION_7_8).build()
+            AppDatabase.MIGRATION_7_8,
+            AppDatabase.MIGRATION_8_9).build()
         database = result
         return result
     }
