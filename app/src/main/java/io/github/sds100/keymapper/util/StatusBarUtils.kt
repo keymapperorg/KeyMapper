9af5c7e5375f31cdc5ce5709a91bbc42ef20fb31
sds100 <developer.sds100@gmail.com>
2021-01-08 15:31:10 +0000

    fix (StatusBarUtils) remove unnecessary logging

--- app/src/main/java/io/github/sds100/keymapper/util/StatusBarUtils.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/StatusBarUtils.kt
@@ -1,14 +1,11 @@
 package io.github.sds100.keymapper.util
 
-import timber.log.Timber
-
 /**
  * Created by sds100 on 05/11/2018.
  */
 
 object StatusBarUtils {
     fun toggleNotificationDrawer(currentPackageName: String) {
-        Timber.e(currentPackageName)
         if (currentPackageName == "com.android.systemui") {
             collapseStatusBar()
         } else {
