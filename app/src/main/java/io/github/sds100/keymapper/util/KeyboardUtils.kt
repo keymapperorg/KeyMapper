bc07762d86638c23720dcac189480a7b88b188a4
sds100 <developer.sds100@gmail.com>
2021-02-01 03:26:16 +0000

    #564 create settings programmatically

--- app/src/main/java/io/github/sds100/keymapper/util/KeyboardUtils.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/KeyboardUtils.kt
@@ -18,6 +18,7 @@ import io.github.sds100.keymapper.util.PermissionUtils.isPermissionGranted
 import io.github.sds100.keymapper.util.result.*
 import splitties.systemservices.inputMethodManager
 import splitties.toast.toast
+import timber.log.Timber
 
 /**
  * Created by sds100 on 28/12/2018.
@@ -64,11 +65,13 @@ object KeyboardUtils {
 
         if (PermissionUtils.haveWriteSecureSettingsPermission(ctx)) {
             getLastUsedCompatibleImeId(ctx).onSuccess {
+                Timber.e("onsuccess " + it)
                 switchIme(ctx, it)
                 return
             }
 
             getImeId(Constants.PACKAGE_NAME).valueOrNull()?.let {
+                Timber.e("getimeid $it")
                 switchIme(ctx, it)
                 return
             }
