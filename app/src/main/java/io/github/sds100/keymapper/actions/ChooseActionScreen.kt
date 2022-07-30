a84e5e511e02e0475219eb238267766d85a130d9
Seth Schroeder <developer.sds100@gmail.com>
2022-07-30 16:22:22 -0500

    refactor (choose action): move search app bar to different file

--- app/src/main/java/io/github/sds100/keymapper/actions/ChooseActionScreen.kt
+++ app/src/main/java/io/github/sds100/keymapper/actions/ChooseActionScreen.kt
@@ -1,28 +1,16 @@
 package io.github.sds100.keymapper.actions
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterExitState
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Keyboard
-import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -151,7 +139,14 @@ private fun ChooseActionScreen(
     navigateToChooseKeyCode: () -> Unit = {}
 ) {
     Scaffold(modifier, bottomBar = {
-        ChooseActionAppBar(onBack, searchState, setSearchState)
+        SearchAppBar(onBack, searchState, setSearchState) {
+            IconButton(onClick = onBack) {
+                Icon(
+                    Icons.Outlined.ArrowBack,
+                    contentDescription = stringResource(R.string.choose_action_back_content_description)
+                )
+            }
+        }
     }) { padding ->
 
         if (configActionState is ConfigActionState.Finished) {
@@ -256,83 +251,4 @@ private fun ChooseActionList(
             }
         }
     }
-}
-
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimationApi::class)
-@Composable
-private fun ChooseActionAppBar(
-    navigateBack: () -> Unit,
-    searchState: SearchState,
-    setSearchState: (SearchState) -> Unit
-) {
-    val isSearching: Boolean by derivedStateOf { searchState is SearchState.Searching }
-    val onBack = {
-        if (isSearching) {
-            setSearchState(SearchState.Idle)
-        } else {
-            navigateBack()
-        }
-    }
-
-    BackHandler(onBack = onBack)
-
-    val searchFieldFocusRequester = remember { FocusRequester() }
-    val keyboardController = LocalSoftwareKeyboardController.current
-
-    BottomAppBar(
-        icons = {
-            IconButton(onClick = onBack) {
-                Icon(
-                    Icons.Outlined.ArrowBack,
-                    contentDescription = stringResource(R.string.choose_action_back_content_description)
-                )
-            }
-
-            AnimatedVisibility(visible = isSearching, modifier = Modifier.fillMaxWidth()) {
-
-                //show the keyboard when opening for the first time
-                if (transition.currentState == EnterExitState.Visible && transition.targetState == EnterExitState.Visible) {
-                    SideEffect {
-                        searchFieldFocusRequester.requestFocus()
-                        keyboardController?.show()
-                    }
-                }
-
-                val textValue = when (searchState) {
-                    SearchState.Idle -> ""
-                    is SearchState.Searching -> searchState.query
-                }
-
-                TextField(
-                    modifier = Modifier
-                        .padding(start = 16.dp, end = 16.dp)
-                        .focusRequester(searchFieldFocusRequester),
-                    value = textValue,
-                    onValueChange = { setSearchState(SearchState.Searching(it)) },
-                    trailingIcon = {
-                        IconButton(onClick = { setSearchState(SearchState.Searching("")) }) {
-                            Icon(
-                                Icons.Outlined.Close,
-                                contentDescription = stringResource(R.string.clear_search_query_content_description)
-                            )
-                        }
-                    },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search)
-                )
-            }
-        },
-        floatingActionButton = {
-            AnimatedVisibility(visible = !isSearching) {
-                FloatingActionButton(
-                    onClick = { setSearchState(SearchState.Searching("")) },
-                    elevation = BottomAppBarDefaults.FloatingActionButtonElevation,
-                ) {
-                    Icon(
-                        Icons.Outlined.Search,
-                        contentDescription = stringResource(R.string.choose_action_search_content_description)
-                    )
-                }
-            }
-        }
-    )
 }
