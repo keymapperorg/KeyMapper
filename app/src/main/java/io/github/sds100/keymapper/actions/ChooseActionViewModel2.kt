7b85cc0bccb26b86a9adc3ee82f019e15b089ec7
Seth Schroeder <developer.sds100@gmail.com>
2022-07-30 22:56:32 -0500

    feat (choose action): choose actions that don't need extra configuring

--- app/src/main/java/io/github/sds100/keymapper/actions/ChooseActionViewModel2.kt
+++ app/src/main/java/io/github/sds100/keymapper/actions/ChooseActionViewModel2.kt
@@ -127,7 +127,7 @@ class ChooseActionViewModel2 @Inject constructor(
                 ActionId.APP -> {
                     configActionState = ConfigActionState.Screen.ChooseApp
                 }
-                
+
                 ActionId.APP_SHORTCUT -> TODO()
                 ActionId.KEY_EVENT -> TODO()
                 ActionId.TAP_SCREEN -> TODO()
@@ -136,93 +136,227 @@ class ChooseActionViewModel2 @Inject constructor(
                 ActionId.INTENT -> TODO()
                 ActionId.PHONE_CALL -> TODO()
                 ActionId.SOUND -> TODO()
-                ActionId.TOGGLE_BLUETOOTH -> TODO()
-                ActionId.ENABLE_BLUETOOTH -> TODO()
-                ActionId.DISABLE_BLUETOOTH -> TODO()
-                ActionId.TOGGLE_MOBILE_DATA -> TODO()
-                ActionId.ENABLE_MOBILE_DATA -> TODO()
-                ActionId.DISABLE_MOBILE_DATA -> TODO()
-                ActionId.TOGGLE_AUTO_BRIGHTNESS -> TODO()
-                ActionId.DISABLE_AUTO_BRIGHTNESS -> TODO()
-                ActionId.ENABLE_AUTO_BRIGHTNESS -> TODO()
-                ActionId.INCREASE_BRIGHTNESS -> TODO()
-                ActionId.DECREASE_BRIGHTNESS -> TODO()
-                ActionId.TOGGLE_AUTO_ROTATE -> TODO()
-                ActionId.ENABLE_AUTO_ROTATE -> TODO()
-                ActionId.DISABLE_AUTO_ROTATE -> TODO()
-                ActionId.PORTRAIT_MODE -> TODO()
-                ActionId.LANDSCAPE_MODE -> TODO()
-                ActionId.SWITCH_ORIENTATION -> TODO()
+                ActionId.TOGGLE_BLUETOOTH -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Bluetooth.Toggle)
+                }
+                ActionId.ENABLE_BLUETOOTH -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Bluetooth.Enable)
+                }
+                ActionId.DISABLE_BLUETOOTH -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Bluetooth.Disable)
+                }
+                ActionId.TOGGLE_MOBILE_DATA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.MobileData.Toggle)
+                }
+                ActionId.ENABLE_MOBILE_DATA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.MobileData.Enable)
+                }
+                ActionId.DISABLE_MOBILE_DATA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.MobileData.Disable)
+                }
+                ActionId.TOGGLE_AUTO_BRIGHTNESS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Brightness.ToggleAuto)
+                }
+                ActionId.DISABLE_AUTO_BRIGHTNESS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Brightness.DisableAuto)
+                }
+                ActionId.ENABLE_AUTO_BRIGHTNESS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Brightness.EnableAuto)
+                }
+                ActionId.INCREASE_BRIGHTNESS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Brightness.Increase)
+                }
+                ActionId.DECREASE_BRIGHTNESS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Brightness.Decrease)
+                }
+                ActionId.TOGGLE_AUTO_ROTATE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Rotation.ToggleAuto)
+                }
+                ActionId.ENABLE_AUTO_ROTATE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Rotation.EnableAuto)
+                }
+                ActionId.DISABLE_AUTO_ROTATE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Rotation.DisableAuto)
+                }
+                ActionId.PORTRAIT_MODE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Rotation.Portrait)
+                }
+                ActionId.LANDSCAPE_MODE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Rotation.Landscape)
+                }
+                ActionId.SWITCH_ORIENTATION -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Rotation.SwitchOrientation)
+                }
                 ActionId.CYCLE_ROTATIONS -> TODO()
                 ActionId.VOLUME_UP -> TODO()
                 ActionId.VOLUME_DOWN -> TODO()
-                ActionId.VOLUME_SHOW_DIALOG -> TODO()
-                ActionId.CYCLE_RINGER_MODE -> TODO()
+                ActionId.VOLUME_SHOW_DIALOG -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ShowVolumeDialog)
+                }
+                ActionId.CYCLE_RINGER_MODE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.CycleRingerMode)
+                }
                 ActionId.CHANGE_RINGER_MODE -> TODO()
-                ActionId.CYCLE_VIBRATE_RING -> TODO()
+                ActionId.CYCLE_VIBRATE_RING -> {
+                    configActionState = ConfigActionState.Finished(ActionData.CycleVibrateRing)
+                }
                 ActionId.TOGGLE_DND_MODE -> TODO()
                 ActionId.ENABLE_DND_MODE -> TODO()
                 ActionId.DISABLE_DND_MODE -> TODO()
                 ActionId.VOLUME_UNMUTE -> TODO()
                 ActionId.VOLUME_MUTE -> TODO()
                 ActionId.VOLUME_TOGGLE_MUTE -> TODO()
-                ActionId.EXPAND_NOTIFICATION_DRAWER -> TODO()
-                ActionId.TOGGLE_NOTIFICATION_DRAWER -> TODO()
-                ActionId.EXPAND_QUICK_SETTINGS -> TODO()
-                ActionId.TOGGLE_QUICK_SETTINGS -> TODO()
-                ActionId.COLLAPSE_STATUS_BAR -> TODO()
-                ActionId.PAUSE_MEDIA -> TODO()
+                ActionId.EXPAND_NOTIFICATION_DRAWER -> {
+                    configActionState = ConfigActionState.Finished(ActionData.StatusBar.ExpandNotifications)
+                }
+                ActionId.TOGGLE_NOTIFICATION_DRAWER -> {
+                    configActionState = ConfigActionState.Finished(ActionData.StatusBar.ToggleNotifications)
+                }
+                ActionId.EXPAND_QUICK_SETTINGS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.StatusBar.ExpandQuickSettings)
+                }
+                ActionId.TOGGLE_QUICK_SETTINGS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.StatusBar.ToggleQuickSettings)
+                }
+                ActionId.COLLAPSE_STATUS_BAR -> {
+                    configActionState = ConfigActionState.Finished(ActionData.StatusBar.Collapse)
+                }
+                ActionId.PAUSE_MEDIA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.Pause)
+                }
                 ActionId.PAUSE_MEDIA_PACKAGE -> TODO()
-                ActionId.PLAY_MEDIA -> TODO()
+                ActionId.PLAY_MEDIA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.Play)
+                }
                 ActionId.PLAY_MEDIA_PACKAGE -> TODO()
-                ActionId.PLAY_PAUSE_MEDIA -> TODO()
+                ActionId.PLAY_PAUSE_MEDIA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.PlayPause)
+                }
                 ActionId.PLAY_PAUSE_MEDIA_PACKAGE -> TODO()
-                ActionId.NEXT_TRACK -> TODO()
+                ActionId.NEXT_TRACK -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.NextTrack)
+                }
                 ActionId.NEXT_TRACK_PACKAGE -> TODO()
-                ActionId.PREVIOUS_TRACK -> TODO()
+                ActionId.PREVIOUS_TRACK -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.PreviousTrack)
+                }
                 ActionId.PREVIOUS_TRACK_PACKAGE -> TODO()
-                ActionId.FAST_FORWARD -> TODO()
+                ActionId.FAST_FORWARD -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.FastForward)
+                }
                 ActionId.FAST_FORWARD_PACKAGE -> TODO()
-                ActionId.REWIND -> TODO()
+                ActionId.REWIND -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ControlMedia.Rewind)
+                }
                 ActionId.REWIND_PACKAGE -> TODO()
-                ActionId.GO_BACK -> TODO()
-                ActionId.GO_HOME -> TODO()
-                ActionId.OPEN_RECENTS -> TODO()
-                ActionId.TOGGLE_SPLIT_SCREEN -> TODO()
-                ActionId.GO_LAST_APP -> TODO()
-                ActionId.OPEN_MENU -> TODO()
+                ActionId.GO_BACK -> {
+                    configActionState = ConfigActionState.Finished(ActionData.GoBack)
+                }
+                ActionId.GO_HOME -> {
+                    configActionState = ConfigActionState.Finished(ActionData.GoHome)
+                }
+                ActionId.OPEN_RECENTS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.OpenRecents)
+                }
+                ActionId.TOGGLE_SPLIT_SCREEN -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ToggleSplitScreen)
+                }
+                ActionId.GO_LAST_APP -> {
+                    configActionState = ConfigActionState.Finished(ActionData.GoLastApp)
+                }
+                ActionId.OPEN_MENU -> {
+                    configActionState = ConfigActionState.Finished(ActionData.OpenMenu)
+                }
                 ActionId.TOGGLE_FLASHLIGHT -> TODO()
                 ActionId.ENABLE_FLASHLIGHT -> TODO()
                 ActionId.DISABLE_FLASHLIGHT -> TODO()
-                ActionId.ENABLE_NFC -> TODO()
-                ActionId.DISABLE_NFC -> TODO()
-                ActionId.TOGGLE_NFC -> TODO()
-                ActionId.MOVE_CURSOR_TO_END -> TODO()
-                ActionId.TOGGLE_KEYBOARD -> TODO()
-                ActionId.SHOW_KEYBOARD -> TODO()
-                ActionId.HIDE_KEYBOARD -> TODO()
-                ActionId.SHOW_KEYBOARD_PICKER -> TODO()
-                ActionId.TEXT_CUT -> TODO()
-                ActionId.TEXT_COPY -> TODO()
-                ActionId.TEXT_PASTE -> TODO()
-                ActionId.SELECT_WORD_AT_CURSOR -> TODO()
-                ActionId.TOGGLE_AIRPLANE_MODE -> TODO()
-                ActionId.ENABLE_AIRPLANE_MODE -> TODO()
-                ActionId.DISABLE_AIRPLANE_MODE -> TODO()
-                ActionId.SCREENSHOT -> TODO()
-                ActionId.OPEN_VOICE_ASSISTANT -> TODO()
-                ActionId.OPEN_DEVICE_ASSISTANT -> TODO()
-                ActionId.OPEN_CAMERA -> TODO()
-                ActionId.LOCK_DEVICE -> TODO()
-                ActionId.POWER_ON_OFF_DEVICE -> TODO()
-                ActionId.SECURE_LOCK_DEVICE -> TODO()
-                ActionId.CONSUME_KEY_EVENT -> TODO()
-                ActionId.OPEN_SETTINGS -> TODO()
-                ActionId.SHOW_POWER_MENU -> TODO()
-                ActionId.DISMISS_MOST_RECENT_NOTIFICATION -> TODO()
-                ActionId.DISMISS_ALL_NOTIFICATIONS -> TODO()
-                ActionId.ANSWER_PHONE_CALL -> TODO()
-                ActionId.END_PHONE_CALL -> TODO()
+                ActionId.ENABLE_NFC -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Nfc.Enable)
+                }
+                ActionId.DISABLE_NFC -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Nfc.Disable)
+                }
+                ActionId.TOGGLE_NFC -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Nfc.Toggle)
+                }
+                ActionId.MOVE_CURSOR_TO_END -> {
+                    configActionState = ConfigActionState.Finished(ActionData.MoveCursorToEnd)
+                }
+                ActionId.TOGGLE_KEYBOARD -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ToggleKeyboard)
+                }
+                ActionId.SHOW_KEYBOARD -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ShowKeyboard)
+                }
+                ActionId.HIDE_KEYBOARD -> {
+                    configActionState = ConfigActionState.Finished(ActionData.HideKeyboard)
+                }
+                ActionId.SHOW_KEYBOARD_PICKER -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ShowKeyboardPicker)
+                }
+                ActionId.TEXT_CUT -> {
+                    configActionState = ConfigActionState.Finished(ActionData.CutText)
+                }
+                ActionId.TEXT_COPY -> {
+                    configActionState = ConfigActionState.Finished(ActionData.CopyText)
+                }
+                ActionId.TEXT_PASTE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.PasteText)
+                }
+                ActionId.SELECT_WORD_AT_CURSOR -> {
+                    configActionState = ConfigActionState.Finished(ActionData.SelectWordAtCursor)
+                }
+                ActionId.TOGGLE_AIRPLANE_MODE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.AirplaneMode.Toggle)
+                }
+                ActionId.ENABLE_AIRPLANE_MODE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.AirplaneMode.Enable)
+                }
+                ActionId.DISABLE_AIRPLANE_MODE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.AirplaneMode.Disable)
+                }
+                ActionId.SCREENSHOT -> {
+                    configActionState = ConfigActionState.Finished(ActionData.Screenshot)
+                }
+                ActionId.OPEN_VOICE_ASSISTANT -> {
+                    configActionState = ConfigActionState.Finished(ActionData.VoiceAssistant)
+                }
+                ActionId.OPEN_DEVICE_ASSISTANT -> {
+                    configActionState = ConfigActionState.Finished(ActionData.DeviceAssistant)
+                }
+                ActionId.OPEN_CAMERA -> {
+                    configActionState = ConfigActionState.Finished(ActionData.OpenCamera)
+                }
+                ActionId.LOCK_DEVICE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.LockDevice)
+                }
+                ActionId.POWER_ON_OFF_DEVICE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ScreenOnOff)
+                }
+                ActionId.SECURE_LOCK_DEVICE -> {
+                    configActionState = ConfigActionState.Finished(ActionData.SecureLock)
+                }
+                ActionId.CONSUME_KEY_EVENT -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ConsumeKeyEvent)
+                }
+                ActionId.OPEN_SETTINGS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.OpenSettings)
+                }
+                ActionId.SHOW_POWER_MENU -> {
+                    configActionState = ConfigActionState.Finished(ActionData.ShowPowerMenu)
+                }
+                ActionId.DISMISS_MOST_RECENT_NOTIFICATION -> {
+                    configActionState = ConfigActionState.Finished(ActionData.DismissLastNotification)
+                }
+                ActionId.DISMISS_ALL_NOTIFICATIONS -> {
+                    configActionState = ConfigActionState.Finished(ActionData.DismissAllNotifications)
+                }
+                ActionId.ANSWER_PHONE_CALL -> {
+                    configActionState = ConfigActionState.Finished(ActionData.AnswerCall)
+                }
+                ActionId.END_PHONE_CALL -> {
+                    configActionState = ConfigActionState.Finished(ActionData.EndCall)
+                }
             }
         }
     }
