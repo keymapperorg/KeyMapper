c545a606728f33e70908f66351ef35981423e547
Seth Schroeder <developer.sds100@gmail.com>
2023-10-07 14:41:26 +0200

    #1171 fix: saving arrays into intent actions didn't work

--- app/src/main/java/io/github/sds100/keymapper/system/intents/IntentAdapter.kt
+++ app/src/main/java/io/github/sds100/keymapper/system/intents/IntentAdapter.kt
@@ -13,8 +13,12 @@ import io.github.sds100.keymapper.util.Success
 class IntentAdapterImpl(context: Context) : IntentAdapter {
     private val ctx = context.applicationContext
 
-    override fun send(target: IntentTarget, uri: String): Result<*> {
+    override fun send(target: IntentTarget, uri: String, extras: List<IntentExtraModel>): Result<*> {
         val intent = Intent.parseUri(uri, 0)
+
+        extras.forEach { e ->
+            e.type.putInIntent(intent, e.name, e.value)
+        }
 
         try {
             when (target) {
@@ -37,5 +41,5 @@ class IntentAdapterImpl(context: Context) : IntentAdapter {
 }
 
 interface IntentAdapter {
-    fun send(target: IntentTarget, uri: String): Result<*>
+    fun send(target: IntentTarget, uri: String, extras: List<IntentExtraModel>): Result<*>
 }
