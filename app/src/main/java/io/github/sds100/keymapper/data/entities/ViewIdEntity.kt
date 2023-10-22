94b322be3f26256e783003f59e65b01875798f73
Seth Schroeder <developer.sds100@gmail.com>
2023-10-22 10:53:18 +0200

    ViewIdEntity: add JSON serialization key names

--- app/src/main/java/io/github/sds100/keymapper/data/entities/ViewIdEntity.kt
+++ app/src/main/java/io/github/sds100/keymapper/data/entities/ViewIdEntity.kt
@@ -3,23 +3,28 @@ package io.github.sds100.keymapper.data.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 import io.github.sds100.keymapper.data.db.dao.ViewIdDao
 
 @Entity(tableName = ViewIdDao.TABLE_NAME)
 data class ViewIdEntity(
     @PrimaryKey(autoGenerate = true)
+    @SerializedName(NAME_ID)
     val id: Int,
     @ColumnInfo(name = ViewIdDao.KEY_ELEMENT_ID)
+    @SerializedName(NAME_VIEW_ID)
     val viewId: String,
     @ColumnInfo(name = ViewIdDao.KEY_PACKAGE_NAME)
+    @SerializedName(NAME_PACKAGE_NAME)
     val packageName: String,
-    @ColumnInfo(name = ViewIdDao.KEY_FULL_NAME,)
+    @ColumnInfo(name = ViewIdDao.KEY_FULL_NAME)
+    @SerializedName(NAME_FULL_NAME)
     val fullName: String,
 ){
     companion object{
         const val NAME_ID = "id"
-        const val NAME_VIEW_ID = "viewId"
-        const val NAME_PACKAGE_NAME = "packageName"
-        const val NAME_FULL_NAME = "fullName"
+        const val NAME_VIEW_ID = "view_id"
+        const val NAME_PACKAGE_NAME = "package_name"
+        const val NAME_FULL_NAME = "full_name"
     }
 }
