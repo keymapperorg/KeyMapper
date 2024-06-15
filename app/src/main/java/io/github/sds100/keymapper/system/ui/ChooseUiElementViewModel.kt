cb9f7328a176ed7026ded71218f40ccf6e53639a
Seth Schroeder <developer.sds100@gmail.com>
2024-06-15 16:45:36 +0200

    style: fix indents

--- app/src/main/java/io/github/sds100/keymapper/system/ui/ChooseUiElementViewModel.kt
+++ app/src/main/java/io/github/sds100/keymapper/system/ui/ChooseUiElementViewModel.kt
@@ -49,9 +49,10 @@ class ChooseUiElementViewModel(
     )
     val state = _state.asStateFlow()
 
-    private val allUiElementListItems: Flow<List<UiElementInfoListItem>> = recordUiElements.uiElements.map { model ->
-        buildListItems(model)
-    }.flowOn(Dispatchers.Default)
+    private val allUiElementListItems: Flow<List<UiElementInfoListItem>> =
+        recordUiElements.uiElements.map { model ->
+            buildListItems(model)
+        }.flowOn(Dispatchers.Default)
 
     private val _returnResult = MutableSharedFlow<UiElementInfo>()
     val returnResult = _returnResult.asSharedFlow()
@@ -65,22 +66,31 @@ class ChooseUiElementViewModel(
 
             is RecordUiElementsState.Stopped -> getString(R.string.button_label_choose_ui_element_record_button_text_start)
         }
-    }.flowOn(Dispatchers.Default).stateIn(viewModelScope, SharingStarted.Eagerly, getString(R.string.button_label_choose_ui_element_record_button_text_start))
+    }.flowOn(Dispatchers.Default).stateIn(
+        viewModelScope,
+        SharingStarted.Eagerly,
+        getString(R.string.button_label_choose_ui_element_record_button_text_start),
+    )
 
-    val recordDescriptionText: StateFlow<String> = recordUiElements.state.map { recordUiElementsState ->
-        when (recordUiElementsState) {
-            is RecordUiElementsState.CountingDown -> getString(R.string.extra_label_interact_with_screen_element_record_description_text_active)
-            is RecordUiElementsState.Stopped -> getString(R.string.extra_label_interact_with_screen_element_record_description_text_start)
-        }
-    }.flowOn(Dispatchers.Default).stateIn(viewModelScope, SharingStarted.Eagerly, getString(R.string.extra_label_interact_with_screen_element_record_description_text_start))
+    val recordDescriptionText: StateFlow<String> =
+        recordUiElements.state.map { recordUiElementsState ->
+            when (recordUiElementsState) {
+                is RecordUiElementsState.CountingDown -> getString(R.string.extra_label_interact_with_screen_element_record_description_text_active)
+                is RecordUiElementsState.Stopped -> getString(R.string.extra_label_interact_with_screen_element_record_description_text_start)
+            }
+        }.flowOn(Dispatchers.Default).stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            getString(R.string.extra_label_interact_with_screen_element_record_description_text_start),
+        )
 
     private val isRecording: StateFlow<Boolean> =
         recordUiElements.state.map { recordUiElementsState ->
             when (recordUiElementsState) {
-            is RecordUiElementsState.CountingDown -> true
-            is RecordUiElementsState.Stopped -> false
-        }
-    }.flowOn(Dispatchers.Default).stateIn(viewModelScope, SharingStarted.Eagerly, false)
+                is RecordUiElementsState.CountingDown -> true
+                is RecordUiElementsState.Stopped -> false
+            }
+        }.flowOn(Dispatchers.Default).stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     val recordButtonEnabled: StateFlow<Boolean> =
         serviceAdapter.state.map { accessibilityServiceState ->
@@ -90,8 +100,8 @@ class ChooseUiElementViewModel(
     val recordButtonAlpha: StateFlow<Float> =
         serviceAdapter.state.map { accessibilityServiceState ->
             when (accessibilityServiceState) {
-            ServiceState.ENABLED -> 1f
-            else -> 0.5f
+                ServiceState.ENABLED -> 1f
+                else -> 0.5f
             }
         }.flowOn(Dispatchers.Default).stateIn(viewModelScope, SharingStarted.Eagerly, 1f)
 
@@ -143,21 +153,22 @@ class ChooseUiElementViewModel(
         }
     }
 
-    private suspend fun buildListItems(model: List<UiElementInfo>): List<UiElementInfoListItem> = flow {
-        for (uiElementInfo in model) {
-            val icon = getAppIcon(uiElementInfo.packageName).valueOrNull()
-            val appName = getAppName(uiElementInfo.packageName).valueOrNull()
+    private suspend fun buildListItems(model: List<UiElementInfo>): List<UiElementInfoListItem> =
+        flow {
+            for (uiElementInfo in model) {
+                val icon = getAppIcon(uiElementInfo.packageName).valueOrNull()
+                val appName = getAppName(uiElementInfo.packageName).valueOrNull()
 
-            val listItem = UiElementInfoListItem(
-                id = uiElementInfo.fullName,
-                title = uiElementInfo.elementName,
-                subtitle = appName ?: uiElementInfo.packageName,
-                icon = if (icon != null) IconInfo(icon) else null,
-            )
+                val listItem = UiElementInfoListItem(
+                    id = uiElementInfo.fullName,
+                    title = uiElementInfo.elementName,
+                    subtitle = appName ?: uiElementInfo.packageName,
+                    icon = if (icon != null) IconInfo(icon) else null,
+                )
 
-            emit(listItem)
-        }
-    }.flowOn(Dispatchers.Default).toList().sortedBy { it.id.lowercase(Locale.getDefault()) }
+                emit(listItem)
+            }
+        }.flowOn(Dispatchers.Default).toList().sortedBy { it.id.lowercase(Locale.getDefault()) }
 
     override fun onCleared() {
         super.onCleared()
@@ -174,7 +185,12 @@ class ChooseUiElementViewModel(
 
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>) =
-            ChooseUiElementViewModel(resourceProvider, recordUiElements, serviceAdapter, displayAppsUseCase) as T
+            ChooseUiElementViewModel(
+                resourceProvider,
+                recordUiElements,
+                serviceAdapter,
+                displayAppsUseCase,
+            ) as T
     }
 }
 
