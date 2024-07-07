591f32c7885f5f4c4d7d6ff227b4f252c4d0c6b6
sds100 <seth.developer@proton.me>
2024-07-07 23:19:08 +0200

    style: reformat

--- app/src/main/java/io/github/sds100/keymapper/settings/MainSettingsFragment.kt
+++ app/src/main/java/io/github/sds100/keymapper/settings/MainSettingsFragment.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.CreateDocument
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.Lifecycle
