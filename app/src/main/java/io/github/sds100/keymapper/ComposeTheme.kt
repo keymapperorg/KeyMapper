8f57a609f39aa75367d0aa22c0b443924cc68594
sds100 <seth.developer@proton.me>
2024-09-26 22:17:12 +0200

    #1297 use dynamic Material You colors for compose theme

--- app/src/main/java/io/github/sds100/keymapper/ComposeTheme.kt
+++ app/src/main/java/io/github/sds100/keymapper/ComposeTheme.kt
@@ -1,14 +1,18 @@
 package io.github.sds100.keymapper
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -79,7 +83,14 @@ fun KeyMapperTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = if (darkTheme) ComposeTheme.darkScheme else ComposeTheme.lightScheme
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> ComposeTheme.darkScheme
+        else -> ComposeTheme.lightScheme
+    }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
