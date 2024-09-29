7f326577a6a11e0b779b5749943aafb950510eb8
sds100 <seth.developer@proton.me>
2024-09-29 10:42:24 +0200

    #1297 add system bars padding to advanced triggers bottomsheet

--- app/src/main/java/io/github/sds100/keymapper/mappings/keymaps/RecordTriggerButtonRow.kt
+++ app/src/main/java/io/github/sds100/keymapper/mappings/keymaps/RecordTriggerButtonRow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Badge
 import androidx.compose.material3.ButtonDefaults
@@ -48,6 +49,7 @@ fun RecordTriggerButtonRow(
 
     if (showBottomSheet) {
         AdvancedTriggersBottomSheet(
+            modifier = Modifier.systemBarsPadding(),
             onDismissRequest = {
                 showBottomSheet = false
             },
