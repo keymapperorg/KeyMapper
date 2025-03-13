acab3611b7f4989e5b42247447dfe8f4ebd0daad
sds100 <seth.developer@proton.me>
2025-03-12 18:28:43 -0600

    #1407 fix migration for floating layouts database

--- app/src/main/java/io/github/sds100/keymapper/data/migration/Migration13To14.kt
+++ app/src/main/java/io/github/sds100/keymapper/data/migration/Migration13To14.kt
@@ -9,9 +9,13 @@ object Migration13To14 {
             """CREATE TABLE IF NOT EXISTS `floating_layouts` (`uid` TEXT NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`uid`))""",
         )
 
+        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS `index_floating_layouts_name` ON `floating_layouts` (`name`)")
+
         // Create floating button table
         database.execSQL(
-            """CREATE TABLE IF NOT EXISTS `floating_buttons` (`uid` TEXT NOT NULL, `layout_uid` TEXT NOT NULL, `name` TEXT NOT NULL, `icon` TEXT NOT NULL, `button_size` INTEGER NOT NULL, `x` INTEGER NOT NULL, `y` INTEGER NOT NULL, `orientation` TEXT NOT NULL, `display_width` INTEGER NOT NULL, `display_height` INTEGER NOT NULL, PRIMARY KEY(`uid`), FOREIGN KEY(`layout_uid`) REFERENCES `floating_layouts`(`uid`) ON UPDATE NO ACTION ON DELETE CASCADE )""",
+            """CREATE TABLE IF NOT EXISTS `floating_buttons` (`uid` TEXT NOT NULL, `layout_uid` TEXT NOT NULL, `text` TEXT NOT NULL, `button_size` INTEGER NOT NULL, `x` INTEGER NOT NULL, `y` INTEGER NOT NULL, `orientation` TEXT NOT NULL, `display_width` INTEGER NOT NULL, `display_height` INTEGER NOT NULL, PRIMARY KEY(`uid`), FOREIGN KEY(`layout_uid`) REFERENCES `floating_layouts`(`uid`) ON UPDATE NO ACTION ON DELETE CASCADE )""",
         )
+
+        database.execSQL("CREATE INDEX IF NOT EXISTS `index_floating_buttons_layout_uid` ON `floating_buttons` (`layout_uid`)")
     }
 }
