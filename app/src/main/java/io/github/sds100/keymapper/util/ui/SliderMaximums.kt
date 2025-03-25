7349fea2723579194d4d5ae6113ef8d8edc80919
sds100 <seth.developer@proton.me>
2025-03-25 13:17:19 -0600

    adjust vibration duration step size and max

--- app/src/main/java/io/github/sds100/keymapper/util/ui/SliderMaximums.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/ui/SliderMaximums.kt
@@ -11,7 +11,7 @@ object SliderMaximums {
     const val ACTION_HOLD_DOWN_DURATION = 5000
     const val ACTION_REPEAT_LIMIT = 20
 
-    const val VIBRATION_DURATION = 200
+    const val VIBRATION_DURATION = 400
     const val TRIGGER_LONG_PRESS_DELAY = 5000
     const val TRIGGER_DOUBLE_PRESS_DELAY = 5000
     const val TRIGGER_SEQUENCE_TRIGGER_TIMEOUT = 5000
--- app/src/main/java/io/github/sds100/keymapper/util/ui/SliderStepSizes.kt
+++ app/src/main/java/io/github/sds100/keymapper/util/ui/SliderStepSizes.kt
@@ -10,7 +10,7 @@ object SliderStepSizes {
     const val ACTION_REPEAT_DELAY = 100
     const val ACTION_REPEAT_LIMIT = 1
     const val ACTION_HOLD_DOWN_DURATION = 100
-    const val VIBRATION_DURATION = 5
+    const val VIBRATION_DURATION = 20
     const val TRIGGER_LONG_PRESS_DELAY = 100
     const val TRIGGER_DOUBLE_PRESS_DELAY = 100
     const val TRIGGER_SEQUENCE_TRIGGER_TIMEOUT = 100
