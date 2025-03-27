bbc838549fe57bb19ac665c42e856c688efd0ad5
sds100 <seth.developer@proton.me>
2025-03-26 18:28:14 -0600

    fix compilation error

--- app/src/free/java/io/github/sds100/keymapper/floating/FloatingLayoutsScreen.kt
+++ app/src/free/java/io/github/sds100/keymapper/floating/FloatingLayoutsScreen.kt
@@ -3,11 +3,13 @@ package io.github.sds100.keymapper.floating
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.compose.foundation.lazy.LazyListState
 
 @Composable
 fun FloatingLayoutsScreen(
     modifier: Modifier = Modifier,
     viewModel: ListFloatingLayoutsViewModel,
     navController: NavHostController,
+    lazyListState: LazyListState,
 ) {
 }
