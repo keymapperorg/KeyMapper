7a47797d9ed5366cdbc0595ca2cb3f84d5f81eaf
sds100 <seth.developer@proton.me>
2025-04-27 17:55:54 +0200

    fix bottom padding on key map selection bottom sheet

--- app/src/main/java/io/github/sds100/keymapper/home/SelectionBottomSheet.kt
+++ app/src/main/java/io/github/sds100/keymapper/home/SelectionBottomSheet.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
@@ -67,8 +66,7 @@ fun SelectionBottomSheet(
     Surface(
         modifier = modifier
             .widthIn(max = BottomSheetDefaults.SheetMaxWidth)
-            .fillMaxWidth()
-            .navigationBarsPadding(),
+            .fillMaxWidth(),
         shadowElevation = 5.dp,
         shape = BottomSheetDefaults.ExpandedShape,
         tonalElevation = BottomSheetDefaults.Elevation,
