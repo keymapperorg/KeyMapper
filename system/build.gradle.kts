68e1cbeca0ddbf405f384228ee10dd28b5685c9b
sds100 <seth.developer@proton.me>
2025-05-17 14:27:35 +0200

    #1711 set minifyenabled in more modules

--- system/build.gradle.kts
+++ system/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
