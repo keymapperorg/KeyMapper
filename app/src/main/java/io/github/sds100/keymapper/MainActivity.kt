c84fa29026ed7eedeeb388b157bae494ce7d3370
sds100 <seth.developer@proton.me>
2025-05-21 20:00:02 +0200

    #1711 programatically add homefragment to nav app graph

--- app/src/main/java/io/github/sds100/keymapper/MainActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/MainActivity.kt
@@ -2,11 +2,13 @@ package io.github.sds100.keymapper
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.sds100.keymapper.R
 import io.github.sds100.keymapper.base.BaseMainActivity
 import io.github.sds100.keymapper.base.utils.ui.showPopups
 import io.github.sds100.keymapper.databinding.ActivityMainBinding
+import io.github.sds100.keymapper.home.HomeFragment
 
 @AndroidEntryPoint
 class MainActivity : BaseMainActivity() {
@@ -16,6 +18,20 @@ class MainActivity : BaseMainActivity() {
 
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = binding.container.getFragment<NavHostFragment>().navController
+        val fragmentNavigator =
+            navController.navigatorProvider.getNavigator(FragmentNavigator::class.java)
+
+        val homeDest = fragmentNavigator.createDestination().apply {
+            id = R.id.home_fragment
+            setClassName(HomeFragment::class.java.name)
+        }
+
+        navController.graph = navController.navInflater.inflate(R.navigation.nav_base_app).apply {
+            addDestination(homeDest)
+            setStartDestination(R.id.home_fragment)
+        }
 
         viewModel.showPopups(this, binding.coordinatorLayout)
     }
