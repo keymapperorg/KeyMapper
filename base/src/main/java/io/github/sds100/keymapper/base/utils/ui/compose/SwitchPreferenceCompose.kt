018f6f44233d2cbeca8a0cf29ef503f7641df534
sds100 <seth.developer@proton.me>
2025-10-02 20:05:05 +0200

    fix: center align SwitchPreferenceCompose icon

--- base/src/main/java/io/github/sds100/keymapper/base/utils/ui/compose/SwitchPreferenceCompose.kt
+++ base/src/main/java/io/github/sds100/keymapper/base/utils/ui/compose/SwitchPreferenceCompose.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -38,6 +39,7 @@ fun SwitchPreferenceCompose(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             Icon(
+                modifier = Modifier.align(Alignment.CenterVertically),
                 imageVector = icon,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onSurface,
