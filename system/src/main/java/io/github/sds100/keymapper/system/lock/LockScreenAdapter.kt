9e64eed3d25b5be4d8e675bc0069719c95a64c11
sds100 <seth.developer@proton.me>
2025-10-09 19:09:30 +0200

    refactor: remove unnecessary sdk checks

--- system/src/main/java/io/github/sds100/keymapper/system/lock/LockScreenAdapter.kt
+++ system/src/main/java/io/github/sds100/keymapper/system/lock/LockScreenAdapter.kt
@@ -1,7 +1,5 @@
 package io.github.sds100.keymapper.system.lock
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import io.github.sds100.keymapper.common.utils.KMResult
 import kotlinx.coroutines.flow.Flow
 
@@ -9,12 +7,8 @@ import kotlinx.coroutines.flow.Flow
 interface LockScreenAdapter {
     fun secureLockDevice(): KMResult<*>
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     fun isLocked(): Boolean
-
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     fun isLockedFlow(): Flow<Boolean>
-
     fun isLockScreenShowing(): Boolean
     fun isLockScreenShowingFlow(): Flow<Boolean>
 }
