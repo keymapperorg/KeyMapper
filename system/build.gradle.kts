fff5f3436e30bc87ec04b9527e216305a1454b58
sds100 <seth.developer@proton.me>
2025-11-02 14:29:03 +0100

    style: add ktlint to system module

--- system/build.gradle.kts
+++ system/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.dagger.hilt.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.jlleitschuh.gradle.ktlint)
 }
 
 android {
