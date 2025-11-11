bb4028dd1a800346ee4b38f03e03a867833ac3a2
sds100 <seth.developer@proton.me>
2025-11-11 16:58:08 +0100

    fix: do not obfuscate SettingType enum

--- system/src/main/java/io/github/sds100/keymapper/system/settings/SettingType.kt
+++ system/src/main/java/io/github/sds100/keymapper/system/settings/SettingType.kt
@@ -1,8 +1,10 @@
 package io.github.sds100.keymapper.system.settings
 
+import androidx.annotation.Keep
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Keep
 enum class SettingType {
     SYSTEM,
     SECURE,
