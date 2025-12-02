b8ac612e36c9095905313506b6499374dfad6ee0
sds100 <seth.developer@proton.me>
2025-12-02 19:44:55 +0100

    add clock to hilt bindings

--- common/src/main/java/io/github/sds100/keymapper/common/utils/Clock.kt
+++ common/src/main/java/io/github/sds100/keymapper/common/utils/Clock.kt
@@ -1,10 +1,11 @@
 package io.github.sds100.keymapper.common.utils
 
 import android.os.SystemClock
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ClockImpl : Clock {
+class ClockImpl @Inject constructor() : Clock {
     override fun elapsedRealtime(): Long {
         return SystemClock.elapsedRealtime()
     }
