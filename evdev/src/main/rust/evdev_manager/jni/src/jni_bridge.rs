e047178b5053c504a37dff199cf1c9f157f74e72
sds100 <seth.developer@proton.me>
2025-12-06 11:50:49 +0100

    set production log level for system bridge to info

--- evdev/src/main/rust/evdev_manager/jni/src/jni_bridge.rs
+++ evdev/src/main/rust/evdev_manager/jni/src/jni_bridge.rs
@@ -24,11 +24,11 @@ pub extern "system" fn Java_io_github_sds100_keymapper_sysbridge_service_BaseSys
     this: JObject,
 ) {
     android_log::init("KeyMapperSystemBridge").unwrap();
-    // Set log level: Warn for production builds, Debug for debug builds
+    // Set log level: Info for production builds, Debug for debug builds
     let log_level = if cfg!(debug_assertions) {
         LevelFilter::Debug
     } else {
-        LevelFilter::Warn
+        LevelFilter::Info
     };
     log::set_max_level(log_level);
     set_log_panic_hook();
