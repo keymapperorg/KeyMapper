c130d12d3383c28e052bd95f0dc374ca07a666a8
WeaR <114218752+IIIWeaRIII@users.noreply.github.com>
2025-12-31 20:01:37 +0100

    Implement ADB service startup in MainActivity
    Added ADB service startup in MainActivity.

--- app/src/main/java/io/github/sds100/keymapper/MainActivity.kt
+++ app/src/main/java/io/github/sds100/keymapper/MainActivity.kt
@@ -1,6 +1,9 @@
 package io.github.sds100.keymapper
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.NavHostFragment
@@ -12,6 +15,9 @@ import io.github.sds100.keymapper.base.utils.ui.DialogProvider
 import io.github.sds100.keymapper.base.utils.ui.showDialogs
 import javax.inject.Inject
 
+// Import du service ADB
+import com.tpn.adbautoenable.AdbConfigService
+
 @AndroidEntryPoint
 class MainActivity : BaseMainActivity() {
 
@@ -21,12 +27,14 @@ class MainActivity : BaseMainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // 1. DÉMARRAGE DU SERVICE ADB
+        startAdbEngine()
+
+        // 2. INTERFACE KEYMAPPER
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navController = binding.container.getFragment<NavHostFragment>().navController
-        val fragmentNavigator =
-            navController.navigatorProvider.getNavigator(FragmentNavigator::class.java)
+        val fragmentNavigator = navController.navigatorProvider.getNavigator(FragmentNavigator::class.java)
 
         val homeDest = fragmentNavigator.createDestination().apply {
             id = R.id.home_fragment
@@ -39,5 +47,20 @@ class MainActivity : BaseMainActivity() {
         }
 
         dialogProvider.showDialogs(this, binding.coordinatorLayout)
+    }
+
+    private fun startAdbEngine() {
+        try {
+            val serviceIntent = Intent(this, AdbConfigService::class.java)
+            serviceIntent.putExtra("boot_config", false)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceIntent)
+            } else {
+                startService(serviceIntent)
+            }
+            Log.d("ADB_FUSION", "Moteur ADB lancé en tâche de fond.")
+        } catch (e: Exception) {
+            Log.e("ADB_FUSION", "Erreur Service: ${e.message}")
+        }
     }
 }
