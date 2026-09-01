b375aa86acd13550a1377cff512557b9c29e3494
sds100 <seth.developer@proton.me>
2026-09-01 12:35:13 +0200

    update Roboelectric version for SDK 37 support

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -73,6 +73,21 @@ android {
         unitTests {
             isIncludeAndroidResources = true
         }
+
+        // Needed for Roboelectric to work on Java 17+
+        unitTests.all {
+            it.jvmArgs(
+                "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                "--add-opens=java.base/java.util=ALL-UNNAMED",
+                "--add-opens=java.base/java.io=ALL-UNNAMED",
+                "--add-opens=java.base/java.net=ALL-UNNAMED",
+                "--add-opens=java.base/java.security=ALL-UNNAMED",
+                "--add-opens=java.base/java.text=ALL-UNNAMED",
+                "--add-opens=java.base/jdk.internal.access=ALL-UNNAMED",
+                "--add-opens=java.desktop/java.awt.font=ALL-UNNAMED",
+                "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            )
+        }
     }
 }
 
